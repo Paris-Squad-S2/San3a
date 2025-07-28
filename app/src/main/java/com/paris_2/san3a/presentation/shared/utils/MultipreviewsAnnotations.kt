57f406ef95fbf0a749799ae208c11031c26006e8
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-07-28 09:17:21 +0300

    add multi-device preview annotations for light and dark themes

--- app/src/main/java/com/paris_2/san3a/presentation/shared/utils/MultipreviewsAnnotations.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/shared/utils/MultipreviewsAnnotations.kt
@@ -1,31 +1,48 @@
 package com.paris_2.san3a.presentation.shared.utils
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 
 @Preview(
-    name = "Phone",
+    name = "Phone light",
     group = "devices",
     device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480"
 )
 @Preview(
-    name = "Arabic",
+    name = "Phone dark",
+    group = "devices",
+    device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480",
+    uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL
+)
+@Preview(
+    name = "Phone landscape light",
+    group = "devices",
+    device = "spec:shape=Normal,width=640,height=360,unit=dp,dpi=480"
+)
+@Preview(
+    name = "Arabic light",
     locale = "ar",
     group = "devices",
     device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480"
 )
-
+@Preview(
+    name = "Arabic dark",
+    locale = "ar",
+    group = "devices",
+    device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480",
+    uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL
+)
 @Preview(
     name = "Foldable",
     group = "devices",
     device = "spec:shape=Normal,width=673,height=841,unit=dp,dpi=480"
 )
-
 @Preview(
     name = "Tablet",
     group = "devices",
     device = "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480"
 )
 
-@PreviewLightDark
 annotation class PreviewMultiDevices
