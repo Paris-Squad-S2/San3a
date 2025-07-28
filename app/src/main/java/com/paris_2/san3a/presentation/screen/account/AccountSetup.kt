82630f87cbedd4a3b8c199f579ec3e99e041e9b1
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-07-29 00:45:50 +0300

    init AccountSetup.

--- app/src/main/java/com/paris_2/san3a/presentation/screen/account/AccountSetup.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/account/AccountSetup.kt
@@ -1,15 +1,16 @@
 package com.paris_2.san3a.presentation.screen.account
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -20,7 +21,7 @@ import com.paris_2.san3a.presentation.screen.account.components.AccountProgressI
 
 @Composable
 fun AccountSetup(modifier: Modifier = Modifier) {
-    var currentScreen by remember { mutableStateOf(0) }
+    var currentScreen by remember { mutableIntStateOf(0) }
     val stepsCount = 4
     val progress = remember(currentScreen) {
         (currentScreen + 1) / stepsCount.toFloat()
@@ -28,20 +29,27 @@ fun AccountSetup(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
+            .safeContentPadding()
             .padding(horizontal = 16.dp)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
+            horizontalArrangement = Arrangement.Center
         ) {
-            AccountProgressIndicator(progress = progress, modifier = Modifier.width(200.dp))
+            AccountProgressIndicator(
+                progress = progress,
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(top = 30.dp)
+            )
         }
+
 
     }
 }
 
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 private fun AccountSetupPrev() {
     AccountSetup()
