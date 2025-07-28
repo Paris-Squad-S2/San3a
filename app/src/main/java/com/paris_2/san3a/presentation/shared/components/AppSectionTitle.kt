e30825fb52fcdebf0eee391619426bbe491b5157
mohammed-akkad <momoo362514@gmail.com>
2025-07-28 11:23:14 +0300

    update app section title

--- app/src/main/java/com/paris_2/san3a/presentation/shared/components/AppSectionTitle.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/shared/components/AppSectionTitle.kt
@@ -3,6 +3,7 @@ package com.paris_2.san3a.presentation.shared.components
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -49,7 +51,7 @@ fun AppSectionTitle(
                 modifier = Modifier
                     .clickable(
                         indication = null,
-                        interactionSource = null,
+                        interactionSource = remember { MutableInteractionSource() },
                         onClick = onClickShowMore
                     )
                     .padding(end = if(icon != null) 12.dp else 0.dp)
