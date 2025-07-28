4a1df9a7e4e06a87704ae2b01a4523eeede8d579
mohammed-akkad <momoo362514@gmail.com>
2025-07-28 16:09:51 +0300

    add otp register view model

---
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/register/OTPRegisterViewModel.kt
@@ -0,0 +1,37 @@
+package com.paris_2.san3a.presentation.screen.register
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+data class OTPRegisterUiState(
+    val otp: String = "",
+    val phoneNumber: String = "",
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
+)
+
+interface OTPRegisterListenerInteraction {
+    fun onOtpTextChange(otp: String)
+    fun onClickVerify()
+
+}
+class OTPRegisterViewModel: ViewModel(), OTPRegisterListenerInteraction {
+
+    private val _uiState = MutableStateFlow(OTPRegisterUiState())
+    val uiState = _uiState.asStateFlow()
+
+
+    override fun onOtpTextChange(otp: String) {
+        _uiState.update {
+            it.copy(otp = otp)
+        }
+    }
+
+    override fun onClickVerify() {
+        TODO("Not yet implemented")
+    }
+
+
+}
