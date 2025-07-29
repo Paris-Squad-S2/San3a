d438262c0af8c6fb1ca789f201220638f3574428
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-07-29 19:23:46 +0300

    Refactor: Removed LocationWebView component

--- app/src/main/java/com/paris_2/san3a/presentation/screen/account/components/LocationWebView.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/account/components/LocationWebView.kt
@@ -5,6 +5,7 @@ import android.webkit.WebView
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -14,17 +15,5 @@ import androidx.compose.ui.viewinterop.AndroidView
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun LocationWebView(modifier: Modifier = Modifier) {
-    val mapUrl = "https://www.google.com/maps?q=San+Francisco&output=embed"
-    AndroidView(
-        factory = { context ->
-            WebView(context).apply {
-                settings.javaScriptEnabled = true
-                loadUrl(mapUrl)
-            }
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(400.dp)
-            .clip(RoundedCornerShape(24.dp)) // Rounded corners like your image
-    )
+
 }
