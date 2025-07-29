d456be05d01833f7da10ca6687c27abf37a8e132
dina othman <dinaosman581@gmail.com>
2025-07-29 11:51:38 +0300

    add VerifyOtpUseCase

--- app/src/main/java/com/paris_2/san3a/domain/usecase/VerifyOtpUseCase.kt
+++ app/src/main/java/com/paris_2/san3a/domain/usecase/VerifyOtpUseCase.kt
@@ -2,10 +2,10 @@ package com.paris_2.san3a.domain.usecase
 
 import com.paris_2.san3a.domain.repository.AuthRepository
 
-class SendOtpUseCase(
+class VerifyOtpUseCase(
     private val repository: AuthRepository
 ) {
-    suspend operator fun invoke(phoneNumber: String):Result<String>{
-         return repository.sendOtp(phoneNumber)
+    suspend operator fun invoke(verificationId: String, code: String){
+         repository.verifyOtp(verificationId, code)
     }
 }
