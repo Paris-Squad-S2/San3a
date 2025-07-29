253cbf7f44059e6f5eb525131e8e5a0a5ddae24b
dina othman <dinaosman581@gmail.com>
2025-07-29 11:39:24 +0300

    add SendOtpUseCase

---
+++ app/src/main/java/com/paris_2/san3a/domain/usecase/SendOtpUseCase.kt
@@ -0,0 +1,11 @@
+package com.paris_2.san3a.domain.usecase
+
+import com.paris_2.san3a.domain.repository.AuthRepository
+
+class SendOtpUseCase(
+    private val repository: AuthRepository
+) {
+    suspend operator fun invoke(phoneNumber: String){
+         repository.sendOtp(phoneNumber)
+    }
+}
