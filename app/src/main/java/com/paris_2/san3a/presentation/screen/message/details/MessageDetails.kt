a211e8e38a2c5613dcf08ed32cae95b47d95bf63
a7med naser <ad0112727@gmail.com>
2025-07-30 09:00:37 +0300

    add ui and view model for test add image behaviour

---
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/message/details/MessageDetails.kt
@@ -0,0 +1,40 @@
+package com.paris_2.san3a.presentation.screen.message.details
+
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MessageDetails(
+    viewModel: MessagesDetailsViewModel
+) {
+    val imagePickerLauncher  = rememberLauncherForActivityResult(
+        ActivityResultContracts.GetMultipleContents()
+    ) { uris ->
+       viewModel.saveImagesUris(uris)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            modifier = Modifier
+                .padding(top = 80.dp)
+                .align(Alignment.CenterHorizontally),
+            onClick = {
+                imagePickerLauncher.launch("image/*")
+            }
+        ) {
+            Text("Add Images")
+        }
+    }
+}
