4b07659ba3519635fe501ca8f1041fbbcd480165
mohammed-akkad <momoo362514@gmail.com>
2025-07-30 14:37:38 +0300

    update auth use case

--- app/src/main/java/com/paris_2/san3a/domain/usecase/VerifyOtpUseCase.kt
+++ app/src/main/java/com/paris_2/san3a/domain/usecase/VerifyOtpUseCase.kt
@@ -1,12 +1,11 @@
 package com.paris_2.san3a.domain.usecase
 
-import com.google.firebase.auth.AuthResult
 import com.paris_2.san3a.domain.repository.AuthRepository
 
 class VerifyOtpUseCase(
     private val repository: AuthRepository
 ) {
-    suspend operator fun invoke(verificationId: String, code: String): AuthResult {
+    suspend operator fun invoke(verificationId: String, code: String): String {
          return repository.verifyOtp(verificationId, code)
     }
 }
