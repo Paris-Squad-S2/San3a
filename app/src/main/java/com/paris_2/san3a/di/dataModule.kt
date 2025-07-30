fdd98ad0be487ea0dc4a47401762db75d7f24256
mohammed-akkad <momoo362514@gmail.com>
2025-07-30 14:36:25 +0300

    update data module

--- app/src/main/java/com/paris_2/san3a/di/dataModule.kt
+++ app/src/main/java/com/paris_2/san3a/di/dataModule.kt
@@ -1,8 +1,11 @@
 package com.paris_2.san3a.di
 
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.paris_2.san3a.data.service.firestore.FireStoreService
 import com.paris_2.san3a.data.service.firestore.FireStoreServiceImpl
+import com.paris_2.san3a.data.source.remote.auth.AuthRemoteDataSource
+import com.paris_2.san3a.data.source.remote.auth.AuthRemoteDataSourceImpl
 import com.paris_2.san3a.data.source.remote.messages.MessagesRemoteDataSource
 import com.paris_2.san3a.data.source.remote.messages.MessagesRemoteDataSourceImp
 import org.koin.dsl.module
@@ -10,6 +13,8 @@ import org.koin.dsl.module
 val dataModule = module {
     single<FireStoreService> { FireStoreServiceImpl(get()) }
     single { FirebaseFirestore.getInstance() }
+    single { FirebaseAuth.getInstance() }
     single<MessagesRemoteDataSource> { MessagesRemoteDataSourceImp(get()) }
+    single<AuthRemoteDataSource> { AuthRemoteDataSourceImpl(get(),get()) }
 
 }
