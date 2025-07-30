0ab9fe6650660ab66a4891f65c39dd8a7dd86426
a7med naser <ad0112727@gmail.com>
2025-07-30 08:59:41 +0300

    update message content of images

--- app/src/main/java/com/paris_2/san3a/domain/entity/Message.kt
+++ app/src/main/java/com/paris_2/san3a/domain/entity/Message.kt
@@ -1,10 +1,16 @@
 package com.paris_2.san3a.domain.entity
 
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.random.Random
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 data class Message(
-    val id:Int,
-    val time: LocalDateTime,
+    val id:Int = Random.nextInt(1,Int.MAX_VALUE),
+    val time: LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()),
     val senderId:Int,
     val receiverId:Int,
     val chatId: Int,
@@ -14,5 +20,5 @@ data class Message(
 sealed class MessageContent{
     data class Text(val content:String): MessageContent()
     data class Audio(val url:String,val duration:Int,val waves:List<Float>): MessageContent()
-    data class Image(val title:String,val urls:List<String>): MessageContent()
+    data class Image(val uris:List<String>): MessageContent()
 }
