34e769e31ca6a3b26d1bab6acd7566ae62e50b38
a7med naser <ad0112727@gmail.com>
2025-07-30 08:59:56 +0300

    update message repository

--- app/src/main/java/com/paris_2/san3a/domain/repository/MessageRepository.kt
+++ app/src/main/java/com/paris_2/san3a/domain/repository/MessageRepository.kt
@@ -1,5 +1,9 @@
 package com.paris_2.san3a.domain.repository
 
+import androidx.core.net.toUri
+import com.paris_2.san3a.data.source.remote.messages.MessagesRemoteDataSource
+import com.paris_2.san3a.data.source.remote.messages.StorageRemoteDataSource
+import com.paris_2.san3a.data.source.remote.messages.dto.MessageDto
 import com.paris_2.san3a.domain.entity.Message
 import com.paris_2.san3a.domain.entity.MessageContent
 import kotlinx.datetime.TimeZone
@@ -9,7 +13,8 @@ import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
 class MessageRepository(
-    // data source
+    private val messagesRemoteDataSource: MessagesRemoteDataSource,
+    private val storageRemoteDataSource: StorageRemoteDataSource,
 ) {
     fun getMessagesByChatId(chatId: Int): List<Message> {
         return listOf(
@@ -25,7 +30,38 @@ class MessageRepository(
         )
     }
 
-    fun saveMessage(message: Message){
-        // Todo( save message to fire store )
+    suspend fun saveMessage(message: Message) {
+        when (message.messageContent) {
+            is MessageContent.Audio -> null
+            is MessageContent.Image -> {
+                val paths = message.messageContent.uris.map { uri ->
+                    "user${message.receiverId}/chat${message.chatId}/${uri.toUri().path?.substringAfterLast("/").orEmpty()}.jpg"
+                }
+                messagesRemoteDataSource.sendMessage(
+                    message.toImageMessageDto(
+                        storageRemoteDataSource.getImagesByPath(paths)
+                    )
+                )
+                storageRemoteDataSource.saveImagesMessage(
+                    receiverId = message.receiverId,
+                    chatId = message.chatId,
+                    uris = message.messageContent.uris.map { it.toUri() }
+                )
+            }
+
+            is MessageContent.Text -> null
+        }
     }
+}
+
+fun Message.toImageMessageDto(urls: List<String>): MessageDto {
+    return MessageDto(
+        id = id.toString(),
+        chatId = chatId.toString(),
+        senderId = senderId.toString(),
+        receiverId = receiverId.toString(),
+        imageUrls = urls,
+        dateTime = time,
+        seen = seen
+    )
 }
