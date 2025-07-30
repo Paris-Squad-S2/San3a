d748d83331e7328522809b985161f5707da52ca9
dina othman <dinaosman581@gmail.com>
2025-07-30 10:16:41 +0300

    update AuthRepository

--- app/src/main/java/com/paris_2/san3a/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/paris_2/san3a/domain/repository/AuthRepository.kt
@@ -1,6 +1,6 @@
 package com.paris_2.san3a.domain.repository
 
 interface AuthRepository {
-    suspend fun sendOtp(phoneNumber: String):Result<String>
+    suspend fun sendOtp(phoneNumber: String):String
     suspend fun verifyOtp(verificationId: String, code: String)
 }
