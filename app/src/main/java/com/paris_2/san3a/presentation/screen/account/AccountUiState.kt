17af7d7978bffb2e213af8f6d4e0a4e3aafdb229
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-07-31 19:37:56 +0300

    Refactor AccountUiState to include location and adjust user type

--- app/src/main/java/com/paris_2/san3a/presentation/screen/account/AccountUiState.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/account/AccountUiState.kt
@@ -3,22 +3,29 @@ package com.paris_2.san3a.presentation.screen.account
 import com.paris_2.san3a.R
 
 data class AccountScreenUiState(
-    val accountUiState: AccountUiState,
+    val accountUiState: AccountUiState = AccountUiState(),
     val isLoading: Boolean = false,
     val errorMassage: String? = "",
 )
 
 data class AccountUiState(
-    val progressIndicator: Float = 0.25F,
-    val userType: UserType = UserType.CUSTOMER,
+    val pageNumber: Int = 0,
+    val userType: UserType? = null,
     val serviceUiState: List<ServiceUiState> = emptyList(),
-    //TODO add location
+    val locationUiState: LocationUiState = LocationUiState(),
 )
 
 data class ServiceUiState(
     val id: Int = 0,
     val serviceTitle: String = "",
     val isSelected: Boolean = false,
+)
+
+data class LocationUiState(
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val countryName: String = "",
+    val cityName: String = "",
 )
 
 enum class UserType(val displayActor: Int) {
