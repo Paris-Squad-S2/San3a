eb51055d5feb3c79e05c91066cdcbe5b332d5241
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-07-31 18:43:43 +0300

    enhance Message component with audio playback and image support

--- app/src/main/java/com/paris_2/san3a/presentation/screen/messagesDetails/components/Message.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/messagesDetails/components/Message.kt
@@ -2,13 +2,20 @@ package com.paris_2.san3a.presentation.screen.messagesDetails.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,7 +42,10 @@ fun Message(
     time: String? = null,
     isReceived: Boolean = true,
     isSeen: Boolean? = false,
-    images: List<String> = emptyList(),
+    images: List<Painter> = emptyList(),
+    onPlayClick: (() -> Unit)? = null,
+    recordWave: List<Float> = emptyList(),
+    listenRatio: Float = 0f,
 ) {
     Row(
         modifier = modifier,
@@ -56,43 +66,141 @@ fun Message(
         }
         Column(
             modifier = Modifier
-                .weight(1f) //
-                .clip(
-                    RoundedCornerShape(
-                        topStart = Theme.radius.extraLarge,
-                        topEnd = Theme.radius.extraLarge,
-                        bottomEnd = if (isReceived) Theme.radius.extraSmall else Theme.radius.extraLarge,
-                        bottomStart = if (isReceived) Theme.radius.extraLarge else Theme.radius.extraSmall,
-                    )
-                )
-                .background(Theme.colors.background.card),
+                .weight(1f),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
-            text?.let {
-                Text(
-                    text = it,
-                    style = Theme.textStyle.body.medium.regular,
-                    modifier = Modifier.padding(
-                        start = 12.dp,
-                        end = 12.dp,
-                        top = 12.dp,
-                        bottom = 4.dp
-                    ),
-                    color = Theme.colors.shade.secondary,
-                    textAlign = TextAlign.End
-                )
+            Column(
+                modifier = Modifier
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = Theme.radius.extraLarge,
+                            topEnd = Theme.radius.extraLarge,
+                            bottomEnd = if (isReceived) Theme.radius.extraSmall else Theme.radius.extraLarge,
+                            bottomStart = if (isReceived) Theme.radius.extraLarge else Theme.radius.extraSmall,
+                        )
+                    )
+                    .background(if (isReceived) Theme.colors.background.card else Theme.colors.brand.tertiary),
+            ) {
+                text?.let {
+                    Text(
+                        text = it,
+                        style = Theme.textStyle.body.medium.regular,
+                        modifier = Modifier.padding(
+                            start = 12.dp,
+                            end = 12.dp,
+                            top = 12.dp,
+                            bottom = 4.dp
+                        ),
+                        color = Theme.colors.shade.secondary,
+                        textAlign = TextAlign.Start
+                    )
+                }
+                if (images.isNotEmpty()) {
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(minSize = 124.dp),
+                        modifier = Modifier
+                            .padding(12.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    ) {
+                        items(images.size) { index ->
+                            Image(
+                                painter = images[index],
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .size(124.dp)
+                                    .clip(RoundedCornerShape(Theme.radius.large))
+                            )
+                        }
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(
+                            start = 12.dp,
+                            end = 12.dp,
+                            bottom = 12.dp
+                        )
+                        .align(Alignment.End),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    time?.let {
+                        Text(
+                            text = it,
+                            style = Theme.textStyle.body.medium.regular,
+                            color = Theme.colors.shade.tertiary,
+                        )
+                    }
+                    if (isSeen ?: false && !isReceived) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_verified_check_outline),
+                            contentDescription = stringResource(R.string.seen_icon),
+                            tint = Theme.colors.additional.primary.blue,
+                            modifier = Modifier
+                                .size(16.dp)
+                        )
+                    }
+                }
             }
-            time?.let {
-                Text(
-                    text = it,
-                    style = Theme.textStyle.body.medium.regular,
-                    modifier = Modifier.padding(
-                        start = 12.dp,
-                        end = 12.dp,
-                        bottom = 12.dp
-                    ),
-                    color = Theme.colors.shade.tertiary,
-                    textAlign = TextAlign.Start
-                )
+            if (onPlayClick != null) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(48.dp)
+                        .clip(RoundedCornerShape(Theme.radius.full))
+                        .background(
+                            Theme.colors.stroke.primary,
+                            RoundedCornerShape(Theme.radius.full)
+                        )
+                        .padding(1.dp)
+                        .background(
+                            Theme.colors.background.card,
+                            RoundedCornerShape(Theme.radius.full)
+                        )
+                        .padding(vertical = 8.dp, horizontal = 12.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_play_bold),
+                        contentDescription = stringResource(R.string.play_icon),
+                        tint = Theme.colors.shade.secondary,
+                        modifier = Modifier
+                            .size(20.dp)
+                            .clickable(onClick = onPlayClick)
+                    )
+                    if (recordWave.isNotEmpty()) {
+                        Row(
+                            modifier = Modifier.weight(1f),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            recordWave.forEachIndexed { index, wave ->
+                                val ratioIndex = (listenRatio * recordWave.size).toInt()
+                                val waveColor = if (index < ratioIndex) {
+                                    Theme.colors.brand.primary
+                                } else {
+                                    Theme.colors.shade.quaternary
+                                }
+                                Surface(
+                                    modifier = Modifier
+                                        .width(4.dp)
+                                        .height((36 * wave).dp)
+                                        .clip(RoundedCornerShape(Theme.radius.full)),
+                                    color = waveColor
+                                ) {}
+                            }
+                        }
+                    } else {
+                        Text(
+                            text = stringResource(R.string.no_audio_wave_available),
+                            style = Theme.textStyle.body.medium.regular,
+                            color = Theme.colors.shade.secondary,
+                        )
+                    }
+                }
             }
         }
         if (isReceived) {
@@ -117,10 +225,18 @@ fun MessagePreview() {
         Surface(color = Color.Red) {
             Message(
                 imagePainter = painterResource(R.drawable.person_chat),
-//                text = "Hello, this is a sample message to demonstrate the message component in the app. It can be quite long to test text wrapping and alignment properly. Let's see how it looks! This is a test message.",
-                text = "Hello jhgjhcvhjevcjvevcjherreerrerfrfrereferrefrefrfre",
+                text = "Hello, this is a sample message to demonstrate the message component in the app. It can be quite long to test text wrapping and alignment properly. Let's see how it looks! This is a test message.",
                 time = "10:30 AM",
-                isReceived = true,
+                isReceived = false,
+                images = listOf(
+                    painterResource(R.drawable.person_chat),
+                    painterResource(R.drawable.person_chat),
+                    painterResource(R.drawable.person_chat),
+                ),
+                isSeen = true,
+                onPlayClick = { /* Handle play click */ },
+                recordWave = listOf(0.3f, 0.2f, 0.3f, 0.7f, 0.5f, 0.1f, 0.7f, 0.4f, 0.9f, 1.0f,0.3f, 0.2f, 0.3f, 0.7f, 0.5f, 0.1f, 0.7f, 0.4f, 0.9f, 1.0f),
+                listenRatio = 0.5f,
             )
         }
     }
