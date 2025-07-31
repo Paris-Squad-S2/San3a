a913f0bdeea938c304f717ba2f5d0b27911ffb87
a7med naser <ad0112727@gmail.com>
2025-07-31 19:52:39 +0300

    add di for data sources , repositories , use cases and view models

---
+++ app/src/main/java/com/paris_2/san3a/di/useCaseModule.kt
@@ -0,0 +1,12 @@
+package com.paris_2.san3a.di
+
+import com.paris_2.san3a.domain.usecase.GetChatsByUserIdUseCase
+import com.paris_2.san3a.domain.usecase.GetMessagesByChatIdUseCase
+import com.paris_2.san3a.domain.usecase.SendMessageUseCase
+import org.koin.dsl.module
+
+val useCaseModule = module {
+    single { SendMessageUseCase(get()) }
+    single { GetChatsByUserIdUseCase(get()) }
+    single { GetMessagesByChatIdUseCase(get()) }
+}
