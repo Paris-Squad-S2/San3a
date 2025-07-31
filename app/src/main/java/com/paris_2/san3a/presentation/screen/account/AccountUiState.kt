3c0d2d78d8c4350792bfb188479a3527e40d8a2b
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-07-31 15:18:22 +0300

    Add Ui State for account set up

--- app/src/main/java/com/paris_2/san3a/presentation/screen/account/AccountUiState.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/account/AccountUiState.kt
@@ -1,2 +1,27 @@
 package com.paris_2.san3a.presentation.screen.account
 
+import com.paris_2.san3a.R
+
+data class AccountScreenUiState(
+    val accountUiState: AccountUiState,
+    val isLoading: Boolean,
+    val errorMassage: String?,
+)
+
+data class AccountUiState(
+    val progressIndicator: Float,
+    val userType: UserType = UserType.CUSTOMER,
+    val serviceUiState: List<ServiceUiState>,
+    //TODO add location
+)
+
+data class ServiceUiState(
+    val id: Int = 0,
+    val serviceTitle: String = "",
+    val isSelected: Boolean = false,
+)
+
+enum class UserType(val displayActor: Int) {
+    CUSTOMER(R.string.customer),
+    CRAFTSMAN(R.string.craftman)
+}
