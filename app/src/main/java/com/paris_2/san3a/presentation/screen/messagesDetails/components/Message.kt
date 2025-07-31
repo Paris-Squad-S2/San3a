38b4df7b32efd332f66cf8b3ab107ef73476e0e6
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-07-31 18:52:49 +0300

    refactor Message component: extract ProfileImage, MessageContent, and AudioPlayer into separate composables

--- app/src/main/java/com/paris_2/san3a/presentation/screen/messagesDetails/components/Message.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/messagesDetails/components/Message.kt
@@ -53,167 +53,244 @@ fun Message(
         verticalAlignment = Alignment.Bottom,
     ) {
         if (!isReceived) {
-            imagePainter?.let {
-                Image(
-                    painter = imagePainter,
-                    contentDescription = stringResource(R.string.profile_image),
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .size(40.dp)
-                )
-            }
+            ProfileImage(imagePainter)
         }
-        Column(
+        MessageContent(
+            text = text,
+            time = time,
+            isReceived = isReceived,
+            isSeen = isSeen,
+            images = images,
+            onPlayClick = onPlayClick,
+            recordWave = recordWave,
+            listenRatio = listenRatio,
             modifier = Modifier
-                .weight(1f),
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-        ) {
-            Column(
-                modifier = Modifier
-                    .clip(
-                        RoundedCornerShape(
-                            topStart = Theme.radius.extraLarge,
-                            topEnd = Theme.radius.extraLarge,
-                            bottomEnd = if (isReceived) Theme.radius.extraSmall else Theme.radius.extraLarge,
-                            bottomStart = if (isReceived) Theme.radius.extraLarge else Theme.radius.extraSmall,
-                        )
-                    )
-                    .background(if (isReceived) Theme.colors.background.card else Theme.colors.brand.tertiary),
-            ) {
-                text?.let {
-                    Text(
-                        text = it,
-                        style = Theme.textStyle.body.medium.regular,
-                        modifier = Modifier.padding(
-                            start = 12.dp,
-                            end = 12.dp,
-                            top = 12.dp,
-                            bottom = 4.dp
-                        ),
-                        color = Theme.colors.shade.secondary,
-                        textAlign = TextAlign.Start
-                    )
-                }
-                if (images.isNotEmpty()) {
-                    LazyVerticalGrid(
-                        columns = GridCells.Adaptive(minSize = 124.dp),
-                        modifier = Modifier
-                            .padding(12.dp),
-                        verticalArrangement = Arrangement.spacedBy(8.dp),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    ) {
-                        items(images.size) { index ->
-                            Image(
-                                painter = images[index],
-                                contentDescription = null,
-                                contentScale = ContentScale.Crop,
-                                modifier = Modifier
-                                    .size(124.dp)
-                                    .clip(RoundedCornerShape(Theme.radius.large))
-                            )
-                        }
-                    }
-                }
-                Row(
-                    modifier = Modifier
-                        .padding(
-                            start = 12.dp,
-                            end = 12.dp,
-                            bottom = 12.dp
-                        )
-                        .align(Alignment.End),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                ) {
-                    time?.let {
-                        Text(
-                            text = it,
-                            style = Theme.textStyle.body.medium.regular,
-                            color = Theme.colors.shade.tertiary,
-                        )
-                    }
-                    if (isSeen ?: false && !isReceived) {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_verified_check_outline),
-                            contentDescription = stringResource(R.string.seen_icon),
-                            tint = Theme.colors.additional.primary.blue,
-                            modifier = Modifier
-                                .size(16.dp)
-                        )
-                    }
-                }
-            }
-            if (onPlayClick != null) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(48.dp)
-                        .clip(RoundedCornerShape(Theme.radius.full))
-                        .background(
-                            Theme.colors.stroke.primary,
-                            RoundedCornerShape(Theme.radius.full)
-                        )
-                        .padding(1.dp)
-                        .background(
-                            Theme.colors.background.card,
-                            RoundedCornerShape(Theme.radius.full)
-                        )
-                        .padding(vertical = 8.dp, horizontal = 12.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(12.dp),
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_play_bold),
-                        contentDescription = stringResource(R.string.play_icon),
-                        tint = Theme.colors.shade.secondary,
-                        modifier = Modifier
-                            .size(20.dp)
-                            .clickable(onClick = onPlayClick)
-                    )
-                    if (recordWave.isNotEmpty()) {
-                        Row(
-                            modifier = Modifier.weight(1f),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            recordWave.forEachIndexed { index, wave ->
-                                val ratioIndex = (listenRatio * recordWave.size).toInt()
-                                val waveColor = if (index < ratioIndex) {
-                                    Theme.colors.brand.primary
-                                } else {
-                                    Theme.colors.shade.quaternary
-                                }
-                                Surface(
-                                    modifier = Modifier
-                                        .width(4.dp)
-                                        .height((36 * wave).dp)
-                                        .clip(RoundedCornerShape(Theme.radius.full)),
-                                    color = waveColor
-                                ) {}
-                            }
-                        }
-                    } else {
-                        Text(
-                            text = stringResource(R.string.no_audio_wave_available),
-                            style = Theme.textStyle.body.medium.regular,
-                            color = Theme.colors.shade.secondary,
-                        )
-                    }
-                }
-            }
-        }
+                .weight(1f)
+        )
         if (isReceived) {
-            imagePainter?.let {
-                Image(
-                    painter = imagePainter,
-                    contentDescription = stringResource(R.string.profile_image),
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(CircleShape)
+            ProfileImage(imagePainter)
+        }
+    }
+}
+
+@Composable
+fun ProfileImage(imagePainter: Painter?) {
+    imagePainter?.let {
+        Image(
+            painter = imagePainter,
+            contentDescription = stringResource(R.string.profile_image),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+    }
+}
+
+@Composable
+fun MessageContent(
+    text: String?,
+    time: String?,
+    isReceived: Boolean,
+    isSeen: Boolean?,
+    images: List<Painter>,
+    onPlayClick: (() -> Unit)?,
+    recordWave: List<Float>,
+    listenRatio: Float,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        MessageBubble(
+            text = text,
+            time = time,
+            isReceived = isReceived,
+            isSeen = isSeen,
+            images = images
+        )
+        if (onPlayClick != null) {
+            AudioPlayer(
+                onPlayClick = onPlayClick,
+                recordWave = recordWave,
+                listenRatio = listenRatio
+            )
+        }
+    }
+}
+
+@Composable
+fun MessageBubble(
+    text: String?,
+    time: String?,
+    isReceived: Boolean,
+    isSeen: Boolean?,
+    images: List<Painter>
+) {
+    Column(
+        modifier = Modifier
+            .clip(
+                RoundedCornerShape(
+                    topStart = Theme.radius.extraLarge,
+                    topEnd = Theme.radius.extraLarge,
+                    bottomEnd = if (isReceived) Theme.radius.extraSmall else Theme.radius.extraLarge,
+                    bottomStart = if (isReceived) Theme.radius.extraLarge else Theme.radius.extraSmall,
                 )
+            )
+            .background(if (isReceived) Theme.colors.background.card else Theme.colors.brand.tertiary),
+    ) {
+        text?.let {
+            Text(
+                text = it,
+                style = Theme.textStyle.body.medium.regular,
+                modifier = Modifier.padding(
+                    start = 12.dp,
+                    end = 12.dp,
+                    top = 12.dp,
+                    bottom = 4.dp
+                ),
+                color = Theme.colors.shade.secondary,
+                textAlign = TextAlign.Start
+            )
+        }
+        if (images.isNotEmpty()) {
+            ImagesGrid(images)
+        }
+        MessageMeta(
+            time = time,
+            isSeen = isSeen,
+            isReceived = isReceived,
+            modifier = Modifier.align(Alignment.End)
+        )
+    }
+}
+
+@Composable
+fun ImagesGrid(images: List<Painter>) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 124.dp),
+        modifier = Modifier.padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(images.size) { index ->
+            Image(
+                painter = images[index],
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(124.dp)
+                    .clip(RoundedCornerShape(Theme.radius.large))
+            )
+        }
+    }
+}
+
+@Composable
+fun MessageMeta(
+    time: String?,
+    isSeen: Boolean?,
+    isReceived: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .padding(
+                start = 12.dp,
+                end = 12.dp,
+                bottom = 12.dp
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        time?.let {
+            Text(
+                text = it,
+                style = Theme.textStyle.body.medium.regular,
+                color = Theme.colors.shade.tertiary,
+            )
+        }
+        if (isSeen ?: false && !isReceived) {
+            Icon(
+                painter = painterResource(R.drawable.ic_verified_check_outline),
+                contentDescription = stringResource(R.string.seen_icon),
+                tint = Theme.colors.additional.primary.blue,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun AudioPlayer(
+    onPlayClick: () -> Unit,
+    recordWave: List<Float>,
+    listenRatio: Float
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .clip(RoundedCornerShape(Theme.radius.full))
+            .background(
+                Theme.colors.stroke.primary,
+                RoundedCornerShape(Theme.radius.full)
+            )
+            .padding(1.dp)
+            .background(
+                Theme.colors.background.card,
+                RoundedCornerShape(Theme.radius.full)
+            )
+            .padding(vertical = 8.dp, horizontal = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_play_bold),
+            contentDescription = stringResource(R.string.play_icon),
+            tint = Theme.colors.shade.secondary,
+            modifier = Modifier
+                .size(20.dp)
+                .clickable(onClick = onPlayClick)
+        )
+        if (recordWave.isNotEmpty()) {
+            AudioWave(recordWave, listenRatio, Modifier.weight(1f))
+        } else {
+            Text(
+                text = stringResource(R.string.no_audio_wave_available),
+                style = Theme.textStyle.body.medium.regular,
+                color = Theme.colors.shade.secondary,
+            )
+        }
+    }
+}
+
+@Composable
+fun AudioWave(
+    recordWave: List<Float>,
+    listenRatio: Float,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        recordWave.forEachIndexed { index, wave ->
+            val ratioIndex = (listenRatio * recordWave.size).toInt()
+            val waveColor = if (index < ratioIndex) {
+                Theme.colors.brand.primary
+            } else {
+                Theme.colors.shade.quaternary
             }
+            Surface(
+                modifier = Modifier
+                    .width(4.dp)
+                    .height((36 * wave).dp)
+                    .clip(RoundedCornerShape(Theme.radius.full)),
+                color = waveColor
+            ) {}
         }
     }
 }
