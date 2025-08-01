783019d172ab8212a7dc9688d4b3ab4bc4053d2a
mohammed-akkad <momoo362514@gmail.com>
2025-08-01 15:22:03 +0300

    update ui module

--- app/src/main/java/com/paris_2/san3a/MainActivity.kt
+++ app/src/main/java/com/paris_2/san3a/MainActivity.kt
@@ -6,17 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.paris_2.san3a.presentation.San3aScaffold
-import com.paris_2.san3a.presentation.util.ActivityProvider
-import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
-    private val activityProvider: ActivityProvider by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        activityProvider.setCurrentActivity(this)
         setContent {
             San3aScaffold()
         }
