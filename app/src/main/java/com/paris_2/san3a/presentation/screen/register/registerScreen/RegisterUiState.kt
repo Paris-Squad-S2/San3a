dc868d301e9d021a42dcee7ac210e0c219bcd334
Renad_Alalfy <renadalalfy@gmail.com>
2025-08-01 13:28:54 +0300

    Add Egypt to country list and include its flag

--- app/src/main/java/com/paris_2/san3a/presentation/screen/register/registerScreen/RegisterUiState.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/register/registerScreen/RegisterUiState.kt
@@ -6,6 +6,7 @@ import com.paris_2.san3a.presentation.shared.components.Country
 
 object CountryProvider {
     val defaultCountries = listOf(
+        Country("EG", "+20", R.drawable.ic_eg_flag),
         Country("Iraq", "+964", R.drawable.ic_iraq_flag),
         Country("UK", "+44", R.drawable.ic_uk_flag)
     )
@@ -17,6 +18,6 @@ data class RegisterUiState(
     val isCountryDropdownExpanded: Boolean = false,
     val phoneNumber: String = "",
     val isLoading: Boolean = false,
-    val errorMessage: String? = null
+    val errorMessage: String? = null,
 )
 
