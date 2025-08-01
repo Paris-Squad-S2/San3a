49cb13d422c8b0d3c75ebfb47157236c95deaf22
Renad_Alalfy <renadalalfy@gmail.com>
2025-08-02 01:42:05 +0300

    Add Palestine to country list and start network checker in Application

--- app/src/main/java/com/paris_2/san3a/San3aApplication.kt
+++ app/src/main/java/com/paris_2/san3a/San3aApplication.kt
@@ -1,10 +1,12 @@
 package com.paris_2.san3a
 
 import android.app.Application
+import com.paris_2.san3a.data.utils.NetworkConnectionChecker
 import com.paris_2.san3a.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.mp.KoinPlatform.getKoin
 
 class San3aApplication : Application() {
     override fun onCreate() {
@@ -13,6 +15,9 @@ class San3aApplication : Application() {
             androidLogger()
             androidContext(this@San3aApplication)
             modules(appModule)
+
+            val networkChecker: NetworkConnectionChecker = getKoin().get()
+            networkChecker.startChecker()
         }
     }
 }
