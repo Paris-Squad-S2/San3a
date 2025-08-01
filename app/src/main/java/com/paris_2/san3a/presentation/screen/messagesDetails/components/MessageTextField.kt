5cc23bdb00860d8cdd8aa45f5ca62bd8da9245e2
a7med naser <ad0112727@gmail.com>
2025-08-01 20:29:34 +0300

    handle localization

--- app/src/main/java/com/paris_2/san3a/presentation/screen/messagesDetails/components/MessageTextField.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/messagesDetails/components/MessageTextField.kt
@@ -1,6 +1,8 @@
-package com.paris_2.san3a.presentation.screen.messagedetails.component
+package com.paris_2.san3a.presentation.screen.messagesDetails.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,12 +16,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
+import com.paris_2.san3a.R
+import com.paris_2.san3a.presentation.shared.designSystem.theme.San3aTheme
 import com.paris_2.san3a.presentation.shared.designSystem.theme.Theme
 
 @Composable
@@ -29,21 +32,25 @@ fun MessageTextField(
     imageIcon: Painter,
     voiceIcon: Painter,
     sendIcon: Painter,
+    onImageClick: () -> Unit,
+    onVoiceClick: () -> Unit={},
+    onSendClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     BasicTextField(
         value = value,
         onValueChange = onValueChange,
         modifier = modifier
-            .fillMaxWidth()
-            .height(72.dp),
+            .background(Theme.colors.background.card, RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
+            .border(
+                color = Theme.colors.stroke.primary,
+                width = 1.dp,
+                shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)
+            ),
         decorationBox = { innerTextField ->
             Row(
-                modifier = Modifier.fillMaxSize()
-                    .background(Theme.colors.background.card, RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
-                ,
-                verticalAlignment = Alignment
-                    .CenterVertically,
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
                 if (value.isEmpty()) {
                     Box(
@@ -52,7 +59,7 @@ fun MessageTextField(
                             .padding(start = 16.dp)
                     ) {
                         Text(
-                            text = "Write a message...",
+                            text = stringResource(R.string.write_a_message),
                             style = Theme.textStyle.body.medium.regular,
                             color = Theme.colors.shade.tertiary
                         )
@@ -60,20 +67,23 @@ fun MessageTextField(
 
                     Icon(
                         painter = imageIcon,
-                        tint =  Theme.colors.shade.secondary,
-                        contentDescription = "Add Images",
+                        tint = Theme.colors.shade.secondary,
+                        contentDescription = stringResource(R.string.add_images),
                         modifier = Modifier
+                            .padding(end = 30.dp)
+                            .clickable(onClick = onImageClick)
                     )
 
                     Icon(
                         painter = voiceIcon,
-                        tint =  Theme.colors.shade.secondary,
-                        contentDescription = "voice icon",
+                        tint = Theme.colors.shade.secondary,
+                        contentDescription = stringResource(R.string.voice_icon),
                         modifier = Modifier
-                            .weight(1f)
+                            .padding(end = 19.dp)
+                            .clickable(onClick = onVoiceClick)
                     )
 
-                }else{
+                } else {
                     Box(
                         modifier = Modifier
                             .weight(3f)
@@ -83,19 +93,34 @@ fun MessageTextField(
                     }
                     Icon(
                         painter = sendIcon,
-                        contentDescription = "send icon",
+                        tint = Theme.colors.brand.primary,
+                        contentDescription = stringResource(R.string.send_icon),
                         modifier = Modifier
-                            .weight(1f)
+                            .padding(start = 19.dp, end = 19.dp)
+                            .clickable(onClick = onSendClick)
                     )
                 }
             }
         },
-        cursorBrush = SolidColor( Theme.colors.shade.tertiary),
-        textStyle = Theme.textStyle.body.medium.regular,
+        textStyle = Theme.textStyle.body.medium.regular.copy(color = Theme.colors.shade.primary),
     )
 }
 
 @PreviewScreenSizes
 @Composable
 private fun MessageTextFieldPreview() {
+    San3aTheme {
+        MessageTextField(
+            value = "",
+            onValueChange = {},
+            imageIcon = painterResource(R.drawable.ic_image),
+            voiceIcon =  painterResource(R.drawable.ic_voice),
+            sendIcon =  painterResource(R.drawable.ic_send),
+            onImageClick = {},
+            onSendClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+        )
+    }
 }
