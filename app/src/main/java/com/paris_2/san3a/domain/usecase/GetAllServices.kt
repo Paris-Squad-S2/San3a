c0e0a545463d657cab9cebdbd436991a54c9951c
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-01 12:01:10 +0300

    rename GetServices to GetAllServices and update to return Flow<List<Service>>

--- app/src/main/java/com/paris_2/san3a/domain/usecase/GetAllServices.kt
+++ app/src/main/java/com/paris_2/san3a/domain/usecase/GetAllServices.kt
@@ -2,11 +2,12 @@ package com.paris_2.san3a.domain.usecase
 
 import com.paris_2.san3a.domain.entity.Service
 import com.paris_2.san3a.domain.repository.HomeRepository
+import kotlinx.coroutines.flow.Flow
 
-class GetServices(
+class GetAllServices(
     private val repository: HomeRepository
 ) {
-    suspend fun invoke(): List<Service> {
-        return repository.getServices()
+    suspend fun invoke(): Flow<List<Service>> {
+        return repository.getAllServices()
     }
 }
