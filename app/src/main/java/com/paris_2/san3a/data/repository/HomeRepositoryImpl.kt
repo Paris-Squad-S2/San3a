7b3f11f4a07ae900d2c40725ed083c30f0e2a0ab
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-01 12:01:25 +0300

    rename getServices to getAllServices and update to return Flow<List<Service>>

--- app/src/main/java/com/paris_2/san3a/data/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/paris_2/san3a/data/repository/HomeRepositoryImpl.kt
@@ -1,14 +1,18 @@
 package com.paris_2.san3a.data.repository
 
-import com.google.firebase.firestore.FirebaseFirestore
+import com.paris_2.san3a.data.mapper.toEntity
+import com.paris_2.san3a.data.source.remote.service.ServiceRemoteDataSource
 import com.paris_2.san3a.domain.entity.Service
 import com.paris_2.san3a.domain.repository.HomeRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class HomeRepositoryImpl(
-    private val firestore: FirebaseFirestore
+    private val serviceRemoteDataSource: ServiceRemoteDataSource,
 ): HomeRepository {
-    override suspend fun getServices(): List<Service> {
-        TODO("Not yet implemented")
+
+    override suspend fun getAllServices(): Flow<List<Service>> {
+        return serviceRemoteDataSource.getAllServices().map { dtoList -> dtoList.map { it.toEntity() } }
     }
 
     override suspend fun getMostRequestedServices() {
