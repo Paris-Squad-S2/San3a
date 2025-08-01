7160e2406bf4e2daf267b6e6033c9c3f62698552
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-08-01 14:22:11 +0300

    feat: Add location pin and location map view
    This commit introduces a new location pin icon and implements a location map view component.
    
    The `LocationWebView.kt` file now includes a Composable function that displays an OpenStreetMap view with a marker at the specified latitude and longitude. The map view is configured with specific properties to disable zoom buttons, animations, and gestures.
    
    The `dataModule.kt` file has been updated to include `LocationRepositoryImp` and `LocationRepository` for managing location-related data.

--- app/src/main/java/com/paris_2/san3a/presentation/screen/account/components/LocationWebView.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/account/components/LocationWebView.kt
@@ -1,9 +1,13 @@
 package com.paris_2.san3a.presentation.screen.account.components
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.utsman.osmandcompose.MapProperties
 import com.utsman.osmandcompose.Marker
 import com.utsman.osmandcompose.OpenStreetMap
+import com.utsman.osmandcompose.ZoomButtonVisibility
 import com.utsman.osmandcompose.rememberCameraState
 import com.utsman.osmandcompose.rememberMarkerState
 import org.osmdroid.util.GeoPoint
@@ -15,13 +19,25 @@ fun LocationWebView(modifier: Modifier = Modifier, longitude: Double, latitude: 
         geoPoint = GeoPoint(latitude, longitude)
         zoom = 5.0
     }
-
     OpenStreetMap(
+        modifier = modifier.size(200.dp),
         cameraState = cameraState,
+        properties = MapProperties(
+            zoomButtonVisibility = ZoomButtonVisibility.NEVER,
+            isAnimating = false,
+            isFlingEnable = false,
+            isTilesScaledToDpi = false,
+            isUseDataConnection = false,
+            isEnableRotationGesture = false,
+        ),
+        onMapClick = { geoPoint ->
+            cameraState.geoPoint = GeoPoint(geoPoint.latitude, geoPoint.longitude)
+        }
     ) {
         Marker(
             state =
-                rememberMarkerState(geoPoint = GeoPoint(latitude, longitude))
-        )
+                rememberMarkerState(geoPoint = GeoPoint(latitude, longitude)),
+
+            )
     }
 }
