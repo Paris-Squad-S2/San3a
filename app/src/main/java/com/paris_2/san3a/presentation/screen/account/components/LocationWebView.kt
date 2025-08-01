94c10fe897fa0aed4358e74c22afb31f1c7dd1b0
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-08-01 15:12:25 +0300

    Make the LocationWebView component interactive
    The LocationWebView component has been updated to allow users to interact with the map and select a location.
    
    Key changes:
    - Added `aLatitude` and `aLongitude` state variables to store the selected latitude and longitude.
    - Initialized the marker and camera states with these variables.
    - Updated the `onMapClick` listener to update the marker's position and log the selected coordinates.
    - Set the initial latitude and longitude to Cairo's coordinates.

--- app/src/main/java/com/paris_2/san3a/presentation/screen/account/components/LocationWebView.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/account/components/LocationWebView.kt
@@ -1,7 +1,12 @@
 package com.paris_2.san3a.presentation.screen.account.components
 
+import android.util.Log
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.utsman.osmandcompose.MapProperties
@@ -13,12 +18,25 @@ import com.utsman.osmandcompose.rememberMarkerState
 import org.osmdroid.util.GeoPoint
 
 @Composable
-fun LocationWebView(modifier: Modifier = Modifier, longitude: Double, latitude: Double) {
+fun LocationWebView(
+    modifier: Modifier = Modifier,
+    initLan: Double = 30.0444,
+    initLong: Double = 31.2357,
+) {
 
+    var aLatitude by remember { mutableDoubleStateOf(initLan) }
+    var aLongitude by remember { mutableDoubleStateOf(initLong) }
+
+    val markerGeoPoint = rememberMarkerState(
+        geoPoint = GeoPoint(aLatitude, aLongitude)
+    )
     val cameraState = rememberCameraState {
-        geoPoint = GeoPoint(latitude, longitude)
-        zoom = 5.0
+        geoPoint = markerGeoPoint.geoPoint
+        zoom = 15.0
     }
+
+
+
     OpenStreetMap(
         modifier = modifier.size(200.dp),
         cameraState = cameraState,
@@ -31,13 +49,21 @@ fun LocationWebView(modifier: Modifier = Modifier, longitude: Double, latitude: 
             isEnableRotationGesture = false,
         ),
         onMapClick = { geoPoint ->
-            cameraState.geoPoint = GeoPoint(geoPoint.latitude, geoPoint.longitude)
+            markerGeoPoint.geoPoint = geoPoint
+            Log.d(
+                "TAG",
+                "LocationWebView:on click || ${geoPoint.latitude} + ${geoPoint.longitude} "
+            )
+            Log.d(
+                "TAG",
+                "LocationWebView: On Long click ||  ${markerGeoPoint.geoPoint.latitude} + ${markerGeoPoint.geoPoint.longitude} "
+            )
         }
     ) {
         Marker(
             state =
-                rememberMarkerState(geoPoint = GeoPoint(latitude, longitude)),
-
-            )
+                markerGeoPoint,
+        )
     }
+
 }
