20f2dfdf00ec47f0c12907455a318ab05d9755dc
Mohamed Elhanafy <m.elhanafy@otas.net>
2025-08-01 15:02:52 +0300

    refactor navigationModule to use Destinations for Navigator initialization

--- app/src/main/java/com/paris_2/san3a/di/navigationModule.kt
+++ app/src/main/java/com/paris_2/san3a/di/navigationModule.kt
@@ -1,11 +1,10 @@
 package com.paris_2.san3a.di
 
+import com.paris_2.san3a.presentation.navigation.Destinations
 import com.paris_2.san3a.presentation.navigation.Navigator
 import com.paris_2.san3a.presentation.navigation.NavigatorImpl
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val navigationModule = module {
-    singleOf(::NavigatorImpl) { bind<Navigator>() }
+    single <Navigator>{ NavigatorImpl(startGraph = Destinations.Graph1) }
 }
