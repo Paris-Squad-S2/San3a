549e0296b46ca7198a38c208d332ef1c999e6c18
mohammed-akkad <momoo362514@gmail.com>
2025-08-01 15:51:41 +0300

    update otp register view model

--- app/src/main/java/com/paris_2/san3a/presentation/screen/register/otpScreen/OTPRegisterUiState.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/register/otpScreen/OTPRegisterUiState.kt
@@ -1,9 +1,11 @@
 package com.paris_2.san3a.presentation.screen.register.otpScreen
 
+import androidx.annotation.StringRes
+
 data class OTPRegisterUiState(
     val otp: String = "",
     val phoneNumber: String = "",
     val secondLeft: Int = 0,
-    val errorMessage: String? = null,
+    @StringRes val errorMessage: Int? = null,
     val verificationId: String = "",
 )
