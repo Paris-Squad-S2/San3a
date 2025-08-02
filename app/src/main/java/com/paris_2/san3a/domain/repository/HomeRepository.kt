80e14cd15fe6322ea6068d2bff304886f02cd015
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-02 13:13:20 +0300

    Refactor: Update HomeRepository to use MostRequestedServices entity
    The `getMostRequestedServices` function in `HomeRepository` now returns a Flow of `List<MostRequestedServices>` instead of `List<Service>`.

--- app/src/main/java/com/paris_2/san3a/domain/repository/HomeRepository.kt
+++ app/src/main/java/com/paris_2/san3a/domain/repository/HomeRepository.kt
@@ -1,5 +1,6 @@
 package com.paris_2.san3a.domain.repository
 
+import com.paris_2.san3a.domain.entity.MostRequestedServices
 import com.paris_2.san3a.domain.entity.RequestService
 import com.paris_2.san3a.domain.entity.Service
 import kotlinx.coroutines.flow.Flow
@@ -8,6 +9,6 @@ interface HomeRepository {
     fun getAllServices(): Flow<List<Service>>
     fun searchServices(query: String): Flow<List<Service>>
     suspend fun requestService(requestedService: RequestService)
-    fun getMostRequestedServices(): Flow<List<Service>>
+    fun getMostRequestedServices(): Flow<List<MostRequestedServices>>
     fun getAvailableJobs(): Flow<List<RequestService>>
 }
