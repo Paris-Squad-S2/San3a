7ad341c7a3d397694a5b91a87d8f1375267edf16
a7med naser <ad0112727@gmail.com>
2025-08-02 07:23:37 +0300

    handle arabic language in chat card

--- app/src/main/java/com/paris_2/san3a/presentation/screen/messages/component/ChatCard.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/messages/component/ChatCard.kt
@@ -1,12 +1,14 @@
 package com.paris_2.san3a.presentation.screen.messages.component
 
+import android.annotation.SuppressLint
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,16 +24,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.paris_2.san3a.R
 import com.paris_2.san3a.presentation.shared.designSystem.theme.San3aTheme
 import com.paris_2.san3a.presentation.shared.designSystem.theme.Theme
+import com.paris_2.san3a.presentation.screen.messages.utils.ArabicStringMapper.isArabicLanguage
+import com.paris_2.san3a.presentation.screen.messages.utils.ArabicStringMapper.toArabicNumerals
+import com.paris_2.san3a.presentation.screen.messages.utils.ArabicStringMapper.toArabicSafe
 
+
+@SuppressLint("LocalContextConfigurationRead")
 @Composable
 fun ChatCard(
     modifier: Modifier = Modifier,
+    onChatClick: () -> Unit = {},
     name: String,
     imageUrl: String,
     lastMessage: String,
@@ -39,11 +50,7 @@ fun ChatCard(
     dateTime: String,
     isCurrentUserSendLastMessage: Boolean,
 ) {
-    val boxSize = animateDpAsState(
-        targetValue = if (unreadMessagesCount in 0..9) 20.dp else 24.dp,
-    )
-
-    val textMessage = animateIntAsState(
+    val unreadMessagesDisplay = animateIntAsState(
         targetValue = when (unreadMessagesCount) {
             in 0..99 -> unreadMessagesCount
             in Int.MIN_VALUE..-1 -> 0
@@ -52,15 +59,18 @@ fun ChatCard(
     )
 
 
-
     Row(
         modifier = modifier
+            .fillMaxWidth()
+            .height(80.dp)
             .background(color = Theme.colors.background.card, RoundedCornerShape(12.dp))
             .border(
                 color = Theme.colors.shade.quinary,
                 width = 1.dp,
                 shape = RoundedCornerShape(12.dp)
-            ),
+            )
+            .clip(RoundedCornerShape(12.dp))
+            .clickable(onClick = onChatClick),
         verticalAlignment = Alignment.CenterVertically
     ) {
         AsyncImage(
@@ -83,17 +93,39 @@ fun ChatCard(
                     .padding(top = 17.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-
                 Text(
                     text = name,
                     style = Theme.textStyle.body.medium.medium,
-                    color = Theme.colors.shade.primary
+                    color = Theme.colors.shade.primary,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .padding(end = 10.dp)
+                        .weight(2.7f)
                 )
-                Text(
-                    text = dateTime,
-                    style = Theme.textStyle.body.small.regular,
-                    color = Theme.colors.shade.secondary
-                )
+
+                if (isArabicLanguage()) {
+                    Text(
+                        text = dateTime.toArabicSafe(),
+                        style = Theme.textStyle.body.small.regular,
+                        color = Theme.colors.shade.secondary,
+                        textAlign = TextAlign.End,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier
+                            .weight(1f)
+                    )
+                } else {
+                    Text(
+                        text = dateTime,
+                        style = Theme.textStyle.body.small.regular,
+                        color = Theme.colors.shade.secondary,
+                        maxLines = 1,
+                        textAlign = TextAlign.End,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
             }
             Row(
                 modifier = Modifier
@@ -101,36 +133,52 @@ fun ChatCard(
                     .padding(top = 43.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Row {
+                Row(
+                    modifier = Modifier
+                        .padding(end = 10.dp)
+                        .weight(2.6f)
+                ) {
                     AnimatedVisibility(
                         visible = isCurrentUserSendLastMessage
                     ) {
                         Text(
                             text = stringResource(R.string.you),
                             style = Theme.textStyle.body.small.regular,
-                            color = Theme.colors.brand.primary
+                            color = Theme.colors.brand.primary,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
                         )
                     }
                     Text(
                         text = lastMessage,
                         style = Theme.textStyle.body.small.regular,
-                        color = Theme.colors.shade.secondary
+                        color = Theme.colors.shade.secondary,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
                     )
                 }
-                Box(
-                    modifier = Modifier
-                        .size(boxSize.value)
-                        .background(
-                            color = Theme.colors.brand.primary,
-                            shape = CircleShape
-                        ),
-                    contentAlignment = Alignment.Center
+                AnimatedVisibility(
+                    visible = unreadMessagesCount > 0
                 ) {
-                    Text(
-                        text = textMessage.value.toString(),
-                        color = Theme.colors.background.card,
-                        style = Theme.textStyle.label.medium.regular
-                    )
+                    Box(
+                        modifier = Modifier
+                            .size(20.dp)
+                            .background(
+                                color = Theme.colors.brand.primary,
+                                shape = CircleShape
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = if (isArabicLanguage())
+                                unreadMessagesDisplay.value.toString().toArabicNumerals()
+                            else
+                                unreadMessagesDisplay.value.toString(),
+                            color = Theme.colors.background.card,
+                            style = Theme.textStyle.label.medium.regular,
+                            fontSize = 12.sp
+                        )
+                    }
                 }
             }
         }
@@ -141,16 +189,27 @@ fun ChatCard(
 @Composable
 private fun ChatCardPreview() {
     San3aTheme {
-        ChatCard(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp),
-            name = "Ahmed Abdelnasser",
-            imageUrl = "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            lastMessage = "where ara you know",
-            unreadMessagesCount = 6,
-            dateTime = "19:34",
-            isCurrentUserSendLastMessage = true
-        )
+        Column {
+            ChatCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp),
+                name = "Ahmed Abdelnasser",
+                imageUrl = "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                lastMessage = "where ara you know",
+                unreadMessagesCount = 6,
+                dateTime = "19:34",
+                isCurrentUserSendLastMessage = true
+            )
+            ChatCard(
+                name = "حمصلاح المقنع",
+                imageUrl = "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                lastMessage = "انت فين يلاا",
+                unreadMessagesCount = 99,
+                dateTime = "monday",
+                isCurrentUserSendLastMessage = false
+            )
+
+        }
     }
 }
