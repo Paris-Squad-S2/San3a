5a87d7e6e1e348cbe73aac7b65a76d5e312a28e2
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-02 13:13:36 +0300

    Refactor: Change ServiceDto to RequestServiceDto in getMostRequestedServices
    The return type of the `getMostRequestedServices` function in `ServiceRemoteDataSource.kt` has been changed from `Flow<List<ServiceDto>>` to `Flow<List<RequestServiceDto>>`.

--- app/src/main/java/com/paris_2/san3a/data/source/remote/service/ServiceRemoteDataSource.kt
+++ app/src/main/java/com/paris_2/san3a/data/source/remote/service/ServiceRemoteDataSource.kt
@@ -8,6 +8,6 @@ interface ServiceRemoteDataSource {
     fun getAllServices(): Flow<List<ServiceDto>>
     suspend fun requestService(requestedServiceDto: RequestServiceDto)
     fun searchServices(query: String): Flow<List<ServiceDto>>
-    fun getMostRequestedServices(): Flow<List<ServiceDto>>
+    fun getMostRequestedServices(): Flow<List<RequestServiceDto>>
     fun getAvailableJobs(): Flow<List<RequestServiceDto>>
 }
