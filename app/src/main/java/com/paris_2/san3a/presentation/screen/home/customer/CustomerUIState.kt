e137bce2aa33a0dbbfcc7c49c6c403699eed3a0b
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-02 17:05:39 +0300

    Define `CustomerUIState` and `CustomerUiState`
    This commit introduces two data classes:
    - `CustomerHomeUiState`: Represents the overall state of the customer's home screen, including loading status, error messages, and the nested `CustomerUiState`.
    - `CustomerUiState`: Holds specific UI data for the customer's home screen, such as the current user's name, location, a list of most requested services, and a list of available services.

---
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/home/customer/CustomerUIState.kt
@@ -0,0 +1,17 @@
+package com.paris_2.san3a.presentation.screen.home.customer
+
+import com.paris_2.san3a.domain.entity.MostRequestedServices
+import com.paris_2.san3a.domain.entity.Service
+
+data class CustomerHomeUiState(
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val customerUiState: CustomerUiState = CustomerUiState(),
+)
+
+data class CustomerUiState(
+    val currentUserName: String = "",
+    val location: String = "",
+    val mostRequestedServices: List<MostRequestedServices> = emptyList(),
+    val services: List<Service> = emptyList(),
+)
