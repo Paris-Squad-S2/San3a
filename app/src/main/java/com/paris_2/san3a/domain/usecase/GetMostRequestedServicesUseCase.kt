a7333d00d0d028ad328f1071d9676f8584705f4b
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-02 11:55:18 +0300

    refactor GetMostRequestedServicesUseCase to remove userId parameter and return List<Service>

--- app/src/main/java/com/paris_2/san3a/domain/usecase/GetMostRequestedServicesUseCase.kt
+++ app/src/main/java/com/paris_2/san3a/domain/usecase/GetMostRequestedServicesUseCase.kt
@@ -1,13 +1,13 @@
 package com.paris_2.san3a.domain.usecase
 
-import com.paris_2.san3a.domain.entity.MostRequestedServices
+import com.paris_2.san3a.domain.entity.Service
 import com.paris_2.san3a.domain.repository.HomeRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetMostRequestedServicesUseCase(
     private val repository: HomeRepository
 ) {
-    operator fun invoke(userId: String): Flow<List<MostRequestedServices>> {
-        return repository.getMostRequestedServices(userId)
+    operator fun invoke(): Flow<List<Service>> {
+        return repository.getMostRequestedServices()
     }
 }
