7aba9c3f2dbb5c2c3fbbae25ab845667db1c9892
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-02 06:38:07 +0300

    feat: Add requestService and getMostRequestedServices functions to HomeRepository
    This commit introduces two new functions to the `HomeRepository` interface:
    
    - `requestService`: Allows requesting a service.
    - `getMostRequestedServices`: Retrieves the most requested services for a given user.

--- app/src/main/java/com/paris_2/san3a/domain/repository/HomeRepository.kt
+++ app/src/main/java/com/paris_2/san3a/domain/repository/HomeRepository.kt
@@ -1,9 +1,13 @@
 package com.paris_2.san3a.domain.repository
 
+import com.paris_2.san3a.domain.entity.MostRequestedServices
+import com.paris_2.san3a.domain.entity.RequestService
 import com.paris_2.san3a.domain.entity.Service
 import kotlinx.coroutines.flow.Flow
 
 interface HomeRepository {
     fun getAllServices(): Flow<List<Service>>
     fun searchServices(query: String): Flow<List<Service>>
+    suspend fun requestService(requestedService: RequestService)
+    fun getMostRequestedServices(userId: String): Flow<List<MostRequestedServices>>
 }
