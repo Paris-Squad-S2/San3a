1a048ade3a76b4c8b91dd7c5e3cb16962e18faca
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-02 06:36:58 +0300

    Add requestedServiceDto parameter to requestService method
    The requestService method in the ServiceRemoteDataSource interface now accepts a RequestServiceDto object as a parameter. This allows for more detailed information about the service being requested.
    
    Additionally, a new method, getMostRequestedServices, has been added to retrieve a list of the most requested services for a given user.

--- app/src/main/java/com/paris_2/san3a/data/source/remote/service/ServiceRemoteDataSource.kt
+++ app/src/main/java/com/paris_2/san3a/data/source/remote/service/ServiceRemoteDataSource.kt
@@ -1,10 +1,12 @@
 package com.paris_2.san3a.data.source.remote.service
 
 import com.paris_2.san3a.data.source.remote.service.dto.ServiceDto
+import com.paris_2.san3a.data.source.remote.user.dto.RequestServiceDto
 import kotlinx.coroutines.flow.Flow
 
 interface ServiceRemoteDataSource {
     fun getAllServices(): Flow<List<ServiceDto>>
-    suspend fun requestService()
+    suspend fun requestService(requestedServiceDto: RequestServiceDto)
     fun searchServices(query: String): Flow<List<ServiceDto>>
+    fun getMostRequestedServices(userId: String): Flow<List<RequestServiceDto>>
 }
