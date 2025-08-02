58336372b01705c5c5a5834534bc2ceb8290b8a0
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-08-02 03:57:43 +0300

    implement getting cities
    Implemented the `getCitiesInGovernment` function in `LocationRepositoryImp` to fetch cities within a specified governorate and country. This involves creating a `CitiesRequest` and using the `locationRemoteDataSource` to make the API call.
    
    Additionally, a `toCities` mapper function was added to `LocationMapper.kt` to convert the `CitiesDto` from the remote data source into a `Cities` domain entity.

--- app/src/main/java/com/paris_2/san3a/data/repository/LocationRepositoryImp.kt
+++ app/src/main/java/com/paris_2/san3a/data/repository/LocationRepositoryImp.kt
@@ -1,7 +1,9 @@
 package com.paris_2.san3a.data.repository
 
+import com.paris_2.san3a.data.mapper.toCities
 import com.paris_2.san3a.data.mapper.toStates
 import com.paris_2.san3a.data.source.remote.location.LocationRemoteDataSource
+import com.paris_2.san3a.data.source.remote.location.request.CitiesRequest
 import com.paris_2.san3a.domain.entity.Cities
 import com.paris_2.san3a.domain.entity.States
 import com.paris_2.san3a.domain.repository.LocationRepository
@@ -9,15 +11,17 @@ import com.paris_2.san3a.domain.repository.LocationRepository
 class LocationRepositoryImp(
     private val locationRemoteDataSource: LocationRemoteDataSource,
 ) : LocationRepository, BaseRepository() {
-    override suspend fun getGovernmentsInCountry(countryName: String): States {
-        return locationRemoteDataSource.getGovernmentsInCountry(countryName).toStates()
-    }
+    override suspend fun getGovernmentsInCountry(countryName: String): States =
+        locationRemoteDataSource.getGovernmentsInCountry(countryName).toStates()
+
 
     override suspend fun getCitiesInGovernment(
         countryName: String,
         governmentName: String,
-    ): Cities {
-        TODO("Not yet implemented")
-    }
-
+    ): Cities = locationRemoteDataSource.getCitiesInGovernment(
+        CitiesRequest(
+            countryName,
+            governmentName
+        )
+    ).toCities()
 }
