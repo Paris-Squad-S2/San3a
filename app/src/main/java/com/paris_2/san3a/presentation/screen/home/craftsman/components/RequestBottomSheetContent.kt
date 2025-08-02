30290b67890a4f5e9f67ae1dd40e1210f647b07f
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-03 02:53:17 +0300

    Add button activation check to back navigation in RequestBottomSheetContent

--- app/src/main/java/com/paris_2/san3a/presentation/screen/home/craftsman/components/RequestBottomSheetContent.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/home/craftsman/components/RequestBottomSheetContent.kt
@@ -14,6 +14,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -80,7 +84,9 @@ fun RequestBottomSheetContent(
                             .background(
                                 Theme.colors.background.bottomSheetCard
                             )
-                            .clickable { onClickBack() }
+                            .clickable {
+                                if (buttonIsActive) { onClickBack()}
+                            }
                     ) {
                         Icon(
                             painter = painterResource(R.drawable.ic_alt_arrow_left_outline),
