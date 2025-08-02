9cc24b359f8d68f6aebb753606811647960334b9
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-08-02 03:09:41 +0300

    Implements location remote data source
    This commit introduces the implementation of `LocationRemoteDataSource` and the `LocationService` interface.
    
    - `LocationService`: Defines methods to fetch states (governments) and cities from a remote API.
    - `LocationRemoteDataSourceImp`: Implements `LocationRemoteDataSource` using `LocationService` to provide functions for getting governments in a country and cities in a government.

---
+++ app/src/main/java/com/paris_2/san3a/data/service/location/LocationService.kt
@@ -0,0 +1,16 @@
+package com.paris_2.san3a.data.service.location
+
+import com.paris_2.san3a.data.source.remote.location.dto.CitiesDto
+import com.paris_2.san3a.data.source.remote.location.dto.StatesDto
+import com.paris_2.san3a.data.source.remote.location.request.CitiesRequest
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface LocationService {
+    @POST("states")
+    suspend fun getStates(@Body body: Map<String, String>): StatesDto
+
+    @POST("state/cities")
+    suspend fun getCities(@Body body: CitiesRequest): CitiesDto
+
+}
--- app/src/main/java/com/paris_2/san3a/data/source/remote/location/LocationRemoteDataSourceImp.kt
+++ app/src/main/java/com/paris_2/san3a/data/source/remote/location/LocationRemoteDataSourceImp.kt
@@ -1,4 +1,18 @@
 package com.paris_2.san3a.data.source.remote.location
 
-class LocationRemoteDataSourceImp: LocationRemoteDataSource {
+import com.paris_2.san3a.data.service.location.LocationService
+import com.paris_2.san3a.data.source.remote.location.dto.CitiesDto
+import com.paris_2.san3a.data.source.remote.location.dto.StatesDto
+import com.paris_2.san3a.data.source.remote.location.request.CitiesRequest
+
+class LocationRemoteDataSourceImp(private val locationService: LocationService) :
+    LocationRemoteDataSource {
+
+    override suspend fun getGovernmentsInCountry(countryName: String): StatesDto =
+        locationService.getStates(mapOf("country" to countryName))
+
+
+    override suspend fun getCitiesInGovernment(citiesRequest: CitiesRequest): CitiesDto =
+        locationService.getCities(body = citiesRequest)
+
 }
