c3ac96286b85a41568acaf6198161d2f5ccf09df
mohammed-akkad <momoo362514@gmail.com>
2025-08-03 14:03:20 +0300

    update auth api services

--- app/src/main/java/com/paris_2/san3a/data/service/auth/AuthApiServices.kt
+++ app/src/main/java/com/paris_2/san3a/data/service/auth/AuthApiServices.kt
@@ -1,5 +1,6 @@
 package com.paris_2.san3a.data.service.auth
 
+import com.paris_2.san3a.BuildConfig
 import com.paris_2.san3a.data.source.remote.auth.dto.OtpDto
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -13,5 +14,6 @@ interface AuthApiServices {
 
 data class WhatsAppMessage(
     val phoneNumber: String,
-    val message: String
+    val message: String,
+    val sessionId: String = BuildConfig.SESSION_ID
 )
