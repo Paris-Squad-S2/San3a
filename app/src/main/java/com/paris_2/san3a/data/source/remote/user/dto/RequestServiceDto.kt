a80b95f7015349e4c0975745ad71d14eb9fd7831
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-03 07:00:34 +0300

    Feat: Update RequestServiceDto to include serviceType, customerComplain, locationDetails, and image

--- app/src/main/java/com/paris_2/san3a/data/source/remote/user/dto/RequestServiceDto.kt
+++ app/src/main/java/com/paris_2/san3a/data/source/remote/user/dto/RequestServiceDto.kt
@@ -4,9 +4,12 @@ import com.google.firebase.firestore.DocumentId
 
 data class RequestServiceDto(
     @DocumentId val id: String,
-    val title: String,
+    val serviceType: String,
+    val customerComplain : String,
     val description: String,
     val location: String,
+    val locationDetails: String,
+    val image : List<Int>,
     val relatedJob: String,
     val offers: List<Double>,
     val userId: String,
@@ -16,9 +19,13 @@ data class RequestServiceDto(
         fun fromJson(data: Map<String, Any>, id: String): RequestServiceDto {
             return RequestServiceDto(
                 id = id,
-                title = data["title"] as? String ?: "",
+                serviceType = data["title"] as? String ?: "",
+                customerComplain = data["customerComplain"] as? String ?: "",
                 description = data["description"] as? String ?: "",
                 location = data["location"] as? String ?: "",
+                locationDetails = data["locationDetails"] as? String ?: "",
+                image = (data["image"] as? List<*>)
+                    ?.mapNotNull { (it as? Number)?.toInt() } ?: emptyList(),
                 relatedJob = data["relatedJob"] as? String ?: "",
                 offers = (data["offers"] as? List<*>)
                     ?.mapNotNull { (it as? Number)?.toDouble() } ?: emptyList(),
