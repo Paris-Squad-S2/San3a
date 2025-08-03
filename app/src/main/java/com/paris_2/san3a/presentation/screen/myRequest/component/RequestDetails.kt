320982d323567b436466fd95bccb33d07fcaf4fa
mohammed-akkad <momoo362514@gmail.com>
2025-08-03 15:17:19 +0300

    update request details

--- app/src/main/java/com/paris_2/san3a/presentation/screen/myRequest/component/RequestDetails.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/myRequest/component/RequestDetails.kt
@@ -2,12 +2,15 @@ package com.paris_2.san3a.presentation.screen.myRequest.component
 
 import androidx.annotation.StringRes
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateOffsetAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -51,28 +54,70 @@ fun orderTrackItems(currentStep: Int): List<OrderTrackItem> {
 
 @Composable
 fun RequestDetails(currentStep: Int = 0) {
-    Row {
-        Column(
-            modifier = Modifier.fillMaxWidth()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center
-        ) {
-            val items = orderTrackItems(currentStep)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        val items = orderTrackItems(currentStep)
 
-            items.forEachIndexed { index, item ->
-                Row(
+        items.forEachIndexed { index, item ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val backgroundIcon = animateColorAsState(
+                    if (item.isDone) Theme.colors.background.card
+                    else Theme.colors.shade.tertiary
+                )
+
+                Box(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
+                        .size(40.dp)
+                        .background(
+                            color = animateColorAsState(
+                                if (item.isDone) Theme.colors.brand.primary
+                                else Theme.colors.button.disabled
+                            ).value,
+                            shape = RoundedCornerShape(Theme.radius.full)
+                        )
+                        .clip(RoundedCornerShape(Theme.radius.full)),
+                    contentAlignment = Alignment.Center
                 ) {
-                    val backgroundIcon = animateColorAsState(
-                        if (item.isDone) Theme.colors.background.card
-                        else Theme.colors.shade.tertiary
+                    Icon(
+                        painter = painterResource(item.icon),
+                        contentDescription = null,
+                        tint = backgroundIcon.value,
+                        modifier = Modifier.size(16.dp)
                     )
+                }
+                val textColor = animateColorAsState(
+                    if (item.isDone) Theme.colors.brand.primary else Theme.colors.shade.tertiary
+                )
+                Text(
+                    text = stringResource(item.status),
+                    style = Theme.textStyle.body.small.semibold,
+                    color = textColor.value,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 8.dp)
+                )
 
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Bottom,
+                    modifier = Modifier.offset(y = (20).dp)
+                ) {
                     Box(
                         modifier = Modifier
-                            .size(40.dp)
+                            .size(12.dp)
+                            .offset(
+                                x = 0.dp,
+                                y = animateDpAsState(if (items.lastIndex == index) (-15).dp else 0.dp).value
+                            )
                             .background(
                                 color = animateColorAsState(
                                     if (item.isDone) Theme.colors.brand.primary
@@ -81,64 +126,27 @@ fun RequestDetails(currentStep: Int = 0) {
                                 shape = RoundedCornerShape(Theme.radius.full)
                             )
                             .clip(RoundedCornerShape(Theme.radius.full)),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Icon(
-                            painter = painterResource(item.icon),
-                            contentDescription = null,
-                            tint = backgroundIcon.value,
-                            modifier = Modifier.size(16.dp)
+                    )
+                    if (index < items.size - 1) {
+                        VerticalDivider(
+                            modifier = Modifier.size(width = 2.dp, height = 40.dp),
+                            color = animateColorAsState(
+                                if (item.isDone) Theme.colors.brand.primary
+                                else Theme.colors.shade.quaternary
+                            ).value,
+                            thickness = 2.dp
                         )
                     }
-                    val textColor = animateColorAsState(
-                        if(item.isDone) Theme.colors.brand.primary else Theme.colors.shade.tertiary
-                    )
-                    Text(
-                        text = stringResource(item.status),
-                        style = Theme.textStyle.body.small.semibold,
-                        color = textColor.value,
-                        modifier = Modifier.weight(1f).padding(start = 8.dp)
-                    )
-
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Bottom,
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .size(12.dp)
-                                .background(
-                                    color = animateColorAsState(
-                                        if (item.isDone) Theme.colors.brand.primary
-                                        else Theme.colors.button.disabled
-                                    ).value,
-                                    shape = RoundedCornerShape(Theme.radius.full)
-                                )
-                                .clip(RoundedCornerShape(Theme.radius.full)),
-                        )
-                        if(index < items.size - 1) {
-                            VerticalDivider(
-                                modifier = Modifier.size(width = 2.dp, height = 40.dp),
-                                color = animateColorAsState(
-                                    if (item.isDone) Theme.colors.brand.primary
-                                    else Theme.colors.shade.quaternary
-                                ).value,
-                                thickness = 2.dp
-                            )
-                        }
-
-                    }
-
-
 
                 }
 
 
-
             }
+
+
         }
     }
+
 
 }
 
