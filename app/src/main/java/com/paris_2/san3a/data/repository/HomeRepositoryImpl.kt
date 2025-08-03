2093647c5b8de98073de3e0fbb44c8653f30b1f3
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-03 23:24:38 +0300

    feat: update HomeRepository to handle service requests and add update functionality

--- app/src/main/java/com/paris_2/san3a/data/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/paris_2/san3a/data/repository/HomeRepositoryImpl.kt
@@ -3,14 +3,13 @@ package com.paris_2.san3a.data.repository
 import androidx.core.net.toUri
 import com.paris_2.san3a.data.mapper.toDto
 import com.paris_2.san3a.data.mapper.toEntity
-import com.paris_2.san3a.data.mapper.toMostRequestedServices
 import com.paris_2.san3a.data.source.remote.service.ServiceRemoteDataSource
 import com.paris_2.san3a.data.source.remote.storage.StorageRemoteDataSource
 import com.paris_2.san3a.domain.GetAllServicesException
 import com.paris_2.san3a.domain.GetAvailableJobsException
 import com.paris_2.san3a.domain.GetMostRequestedServicesException
 import com.paris_2.san3a.domain.SearchServicesException
-import com.paris_2.san3a.domain.entity.MostRequestedServices
+import com.paris_2.san3a.domain.UpdateNumOfRequestsException
 import com.paris_2.san3a.domain.entity.RequestService
 import com.paris_2.san3a.domain.entity.Service
 import com.paris_2.san3a.domain.repository.HomeRepository
@@ -36,27 +35,32 @@ class HomeRepositoryImpl(
     }
 
     override suspend fun requestService(requestedService: RequestService) {
-        try{
-            val paths = requestedService.image.map { uri ->
-                "${requestedService.title}/${
-                    uri.toUri().path?.substringAfterLast(
-                        "/"
-                    ).orEmpty()
-                }.jpg"
+        try {
+            val imageUris = requestedService.image
+            val imageUrls = if (imageUris.isNotEmpty()) {
+                val paths = imageUris.map { uri ->
+                    "${requestedService.title}/${
+                        uri.toUri().path?.substringAfterLast("/") ?: ""
+                    }.jpg"
+                }
+                firebaseStorageRemoteDataSource.saveImages(
+                    paths,
+                    imageUris.map { it.toUri() }
+                )
+                firebaseStorageRemoteDataSource.getImagesByPaths(paths)
+            } else {
+                emptyList()
             }
-            firebaseStorageRemoteDataSource.saveImages(
-                paths,
-                requestedService.image.map { it.toUri() })
-            val imageUrls = firebaseStorageRemoteDataSource.getImagesByPaths(paths)
+
             serviceRemoteDataSource.requestService(requestedService.toDto(imageUrls))
-        }catch (e: Exception){
+        } catch (e: Exception) {
             throw e
         }
     }
 
-    override fun getMostRequestedServices(): Flow<List<MostRequestedServices>> {
+    override fun getMostRequestedServices(): Flow<List<Service>> {
         return serviceRemoteDataSource.getMostRequestedServices()
-            .map { dto -> dto.map { it.toMostRequestedServices() } }
+            .map { dto -> dto.map { it.toEntity() } }
             .catch { throw GetMostRequestedServicesException() }
 
     }
@@ -65,6 +69,14 @@ class HomeRepositoryImpl(
         return serviceRemoteDataSource.getAvailableJobs()
             .map { dto -> dto.map { it.toEntity() } }
             .catch { throw GetAvailableJobsException() }
+    }
+
+    override suspend fun updateNumOfRequestService(serviceId: String) {
+        return try {
+            serviceRemoteDataSource.updateNumOfRequestService(serviceId)
+        }catch (e: Exception) {
+            throw UpdateNumOfRequestsException()
+        }
     }
 
 
