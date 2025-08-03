9010a7a2de5d2d425354d22e92b8a673a60311f1
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-04 02:19:24 +0300

    refactor: update messages feature to use a map for chat data and improve UI handling

--- app/src/main/java/com/paris_2/san3a/presentation/screen/messages/MessagesScreen.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/messages/MessagesScreen.kt
@@ -3,23 +3,19 @@ package com.paris_2.san3a.presentation.screen.messages
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.paris_2.san3a.R
-import com.paris_2.san3a.domain.entity.Chat
 import com.paris_2.san3a.presentation.screen.messages.component.ChatCard
 import com.paris_2.san3a.presentation.shared.components.AppBar
 import com.paris_2.san3a.presentation.shared.components.AppScaffold
@@ -44,58 +40,59 @@ private fun MessagesScreenContent(
     modifier: Modifier = Modifier,
     state: MessagesState
 ) {
-    Column(
-        modifier = modifier
+    AppScaffold(
+        modifier = Modifier
             .fillMaxSize()
             .background(Theme.colors.background.card)
             .statusBarsPadding(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        topBar = {
+            AppBar(
+                title = stringResource(R.string.messages),
+                actionIcon = {
+                    Icon(
+                        modifier = Modifier
+                            .clickable(onClick = messagesInteractionListener::onNotificationClick),
+                        painter = painterResource(R.drawable.ic_notification_outline),
+                        contentDescription = null,
+                        tint = Theme.colors.shade.primary
+                    )
+                }
+            )
+        },
     ) {
-        AppScaffold(
-            topBar = {
-                AppBar(
-                    title = stringResource(R.string.messages),
-                    actionIcon = {
-                        Icon(
-                            modifier = Modifier
-                                .clickable(onClick = messagesInteractionListener::onNotificationClick),
-                            painter = painterResource(R.drawable.ic_notification_outline),
-                            contentDescription = null,
-                            tint = Theme.colors.shade.primary
-                        )
-                    }
-                )
-            },
-        ) {
-            if (state.error != null) {
-                LostConnectionScreen(
-                    onRetry = messagesInteractionListener::onRetryClick,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Theme.colors.background.screen)
-                        .padding(horizontal = 60.dp)
-                )
-            } else if (state.isLoading) {
-                LoadingScreen(
-                    modifier = Modifier.fillMaxSize().background(Theme.colors.background.screen)
-                )
-            } else if (state.chats.isEmpty()) {
-                PlaceHolderScreen(
-                    modifier = modifier
-                        .fillMaxSize()
-                        .background(Theme.colors.background.screen)
-                        .padding(horizontal = 60.dp),
-                    image = R.drawable.img_message,
-                    title = R.string.no_messages_yet,
-                    description = R.string.once_you_start_accepting_or_posting_requests_chats_with_craftsmen_will_appear_here,
-                )
-            } else {
-                ChatList(
-                    modifier = Modifier.fillMaxSize().background(Theme.colors.background.screen).padding(top = 16.dp, start = 16.dp, end = 16.dp),
-                    chats = state.chats,
-                    messagesInteractionListener = messagesInteractionListener
-                )
-            }
+        if (state.error != null) {
+            LostConnectionScreen(
+                onRetry = messagesInteractionListener::onRetryClick,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Theme.colors.background.screen)
+                    .padding(horizontal = 60.dp)
+            )
+        } else if (state.isLoading) {
+            LoadingScreen(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Theme.colors.background.screen)
+            )
+        } else if (state.chatsMap.isEmpty()) {
+            PlaceHolderScreen(
+                modifier = modifier
+                    .fillMaxSize()
+                    .background(Theme.colors.background.screen)
+                    .padding(horizontal = 60.dp),
+                image = R.drawable.img_message,
+                title = R.string.no_messages_yet,
+                description = R.string.once_you_start_accepting_or_posting_requests_chats_with_craftsmen_will_appear_here,
+            )
+        } else {
+            ChatList(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Theme.colors.background.screen)
+                    .padding(top = 16.dp, start = 16.dp, end = 16.dp),
+                chatsMap = state.chatsMap,
+                messagesInteractionListener = messagesInteractionListener
+            )
         }
     }
 }
@@ -103,24 +100,27 @@ private fun MessagesScreenContent(
 @Composable
 fun ChatList(
     modifier: Modifier,
-    chats: List<Chat>,
+    chatsMap: Map<String, ChatUI>,
     messagesInteractionListener: MessagesInteractionListener
 ) {
-
-     LazyColumn(
-         modifier = modifier,
-         verticalArrangement = Arrangement.spacedBy(12.dp),
-     ) {
-         items(chats) { item ->
-             ChatCard(
-                 onChatClick = {messagesInteractionListener.onChatClick(item.id)},
-                 name = "kabanoka kazonga", // Todo
-                 imageUrl = "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                 lastMessage = "where are you", // Todo()
-                 unreadMessagesCount = 99,
-                 dateTime = "9:40",
-                 isCurrentUserSendLastMessage = false // Todo()
-             )
-         }
-     }
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
+        items(chatsMap.size) { item ->
+            chatsMap.keys.elementAt(item).let { chatId ->
+                chatsMap[chatId]?.let { chatUI ->
+                    ChatCard(
+                        onChatClick = { messagesInteractionListener.onChatClick(chatUI.chatId) },
+                        name = chatUI.theOtherUserName ?: "Unknown User",
+                        imageUrl = chatUI.theOtherProfilePhoto ?: "https://example.com/default_image.png",
+                        lastMessage = chatUI.lastMessage,
+                        unreadMessagesCount = chatUI.unreadMessagesCount,
+                        dateTime = chatUI.lastMessageTime,
+                        isCurrentUserSendLastMessage = chatUI.lastMessageSenderId != chatUI.theOtherUserId,
+                    )
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/paris_2/san3a/presentation/screen/messages/MessagesState.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/messages/MessagesState.kt
@@ -1,10 +1,47 @@
 package com.paris_2.san3a.presentation.screen.messages
 
 import com.paris_2.san3a.domain.entity.Chat
+import com.paris_2.san3a.domain.entity.MessageContent
 
 data class MessagesState(
-    val chats: List<Chat> = emptyList(),
+    val chatsMap: Map<String, ChatUI> = emptyMap(),
     val currentUserId: String = "",
     val isLoading: Boolean = false,
     val error: String? = null,
 )
+
+data class ChatUI(
+    val chatId: String,
+    val unreadMessagesCount: Int,
+    val lastMessageTime: String,
+    val lastMessageSenderId: String,
+    val lastMessage: String,
+    val theOtherUserId: String,
+    val theOtherUserName: String? = null,
+    val theOtherProfilePhoto: String? = null,
+)
+
+fun MessageContent.toMessageContentUI(): String {
+    return when (this) {
+        is MessageContent.Text -> this.content
+        is MessageContent.Image -> "[Image]"
+        is MessageContent.Audio -> "[Audio]"
+    }
+}
+
+fun Chat.toChatUI(userId: String): ChatUI {
+    return ChatUI(
+        chatId = this.id,
+        lastMessage = this.lastMessage?.messageContent?.toMessageContentUI() ?: "",
+        lastMessageTime = this.lastMessage?.time?.toString() ?: "",
+        lastMessageSenderId = this.lastMessage?.senderId ?: "",
+        unreadMessagesCount = this.unreadMessagesCount,
+        theOtherUserId = this.usersParticipantIds.firstOrNull { it != userId } ?: "",
+    )
+}
+
+fun List<Chat>.toChatUIMap(userId: String): Map<String, ChatUI> {
+    return this.associate { chat ->
+        chat.id to chat.toChatUI(userId)
+    }
+}
