44945132ccc1555db8193a47100dabfd2b08f926
Mohamed Elhanafy <m.elhanafy@otas.net>
2025-08-03 18:38:41 +0300

    feat: implement GetUserUseCase and GetWorkMediaUseCase with corresponding repository methods

---
+++ app/src/main/java/com/paris_2/san3a/domain/usecase/GetUserUseCase.kt
@@ -0,0 +1,11 @@
+package com.paris_2.san3a.domain.usecase
+
+import com.paris_2.san3a.domain.entity.User
+import com.paris_2.san3a.domain.repository.UserRepository
+
+class GetUserUseCase(
+    private val userRepository: UserRepository
+) {
+    suspend operator fun invoke(phoneNumber: String): User =
+        userRepository.getUser(phoneNumber)
+}
