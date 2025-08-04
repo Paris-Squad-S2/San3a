f7c5dfe25536370594a0f8d0b1523c87492d9d20
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-04 06:07:14 +0300

    Add initBottomSheet method to CustomerHomeInteractionListener for bottom sheet initialization

--- app/src/main/java/com/paris_2/san3a/presentation/screen/home/customer/CustomerHomeInteractionListener.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/home/customer/CustomerHomeInteractionListener.kt
@@ -1,16 +1,13 @@
 package com.paris_2.san3a.presentation.screen.home.customer
 
 interface CustomerHomeInteractionListener {
-
     fun onNotificationClick()
-
     fun onSearch(query: String)
-
     fun onServiceClick(serviceId: String)
-
     fun onBecomeCraftsmanClick()
-
     fun onDismissBottomSheet()
     fun createRequest(service: RequestServiceUiState , serviceId: String)
     fun updateNumOfRequests(serviceId: String)
+    fun initBottomSheet(serviceTitle: String, serviceId: String, iconRes: Int)
+
 }
