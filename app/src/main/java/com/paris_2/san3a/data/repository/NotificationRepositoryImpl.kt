5f69f75e7b6c09d652590c756b9290881d8b1e2f
Zeinab979 <zeinabwahdan3677@gmail.com>
2025-08-04 23:00:19 +0300

    handle exception for notification repository impl

--- app/src/main/java/com/paris_2/san3a/data/repository/NotificationRepositoryImpl.kt
+++ app/src/main/java/com/paris_2/san3a/data/repository/NotificationRepositoryImpl.kt
@@ -7,17 +7,17 @@ import com.paris_2.san3a.domain.NotificationException
 import com.paris_2.san3a.domain.entity.Notification
 import com.paris_2.san3a.domain.repository.NotificationRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 
 class NotificationRepositoryImpl(
     private val notificationDataSource: NotificationDataSource
 ) : NotificationRepository, BaseRepository() {
-
     override fun getStreamNotifications(userId: String): Flow<List<Notification>> {
-
-        return notificationDataSource.getStreamNotifications(userId = userId)
-            .map { dtoList ->
-                dtoList.map { dto -> dto.toDomain() }
+        return notificationDataSource.getStreamNotifications(userId)
+            .map { dtoList -> dtoList.map { it.toDomain() } }
+            .catch { e ->
+                throw NotificationException("Failed to get notifications: ${e.message}")
             }
     }
     override suspend fun addNotification(notification: Notification): String {
