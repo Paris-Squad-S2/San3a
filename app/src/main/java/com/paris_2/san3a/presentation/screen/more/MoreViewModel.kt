d67c90174403120a4e18f2612457d08c8520b8be
mohammed-akkad <momoo362514@gmail.com>
2025-08-04 12:08:11 +0300

    add more view model

---
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/more/MoreViewModel.kt
@@ -0,0 +1,36 @@
+package com.paris_2.san3a.presentation.screen.more
+
+import com.paris_2.san3a.presentation.shared.utils.BaseViewModel
+
+class MoreViewModel(
+
+): BaseViewModel<MoreUiState>(MoreUiState()), MoreInteractionListener {
+    override fun onClickEdit() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickSwitchAccountToCraftsman() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickSwitchAccountToCustomer() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickLanguage() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickLocation() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickLogout() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onChangeToDarkMode(isDarkMode: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+}
