6b875c66119423522cf7151c08f4c4fb8cca9ce4
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-07 21:26:57 +0300

    Add placeholder for empty offers and refactor request info components

--- app/src/main/java/com/paris_2/san3a/presentation/screen/requestDetails/craftsman/CraftsManRequestDetailsScreen.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/requestDetails/craftsman/CraftsManRequestDetailsScreen.kt
@@ -3,10 +3,8 @@ package com.paris_2.san3a.presentation.screen.requestDetails.craftsman
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,7 +15,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -29,8 +26,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -38,6 +33,7 @@ import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import coil3.compose.rememberAsyncImagePainter
 import com.paris_2.san3a.R
+import com.paris_2.san3a.presentation.screen.requestDetails.components.RequestInfoSection
 import com.paris_2.san3a.presentation.shared.components.AppBar
 import com.paris_2.san3a.presentation.shared.components.AppButton
 import com.paris_2.san3a.presentation.shared.components.AppButtonSize
@@ -46,6 +42,7 @@ import com.paris_2.san3a.presentation.shared.components.AppScaffold
 import com.paris_2.san3a.presentation.shared.components.CraftsManOffer
 import com.paris_2.san3a.presentation.shared.components.LoadingScreen
 import com.paris_2.san3a.presentation.shared.components.LostConnectionScreen
+import com.paris_2.san3a.presentation.shared.components.PlaceHolderScreen
 import com.paris_2.san3a.presentation.shared.designSystem.theme.Theme
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -244,6 +241,21 @@ fun CraftsmanRequestDetailsContent(
                 )
             }
         }
+
+        item {
+            AnimatedVisibility(
+                visible = state.offers.isEmpty()
+            ) {
+                PlaceHolderScreen(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 60.dp),
+                    image = R.drawable.img_empty_offers,
+                title = R.string.waiting_for_offers,
+                description = R.string.craftsmen_in_your_area_will_send_offers_soon_you_ll_be_notified_when_they_arrive,
+                )
+            }
+        }
     }
 }
 
@@ -330,118 +342,3 @@ fun AddYourOfferSection(
     }
 }
 
-@Composable
-fun RequestInfoSection(
-    modifier: Modifier = Modifier,
-    request: RequestServiceUIState,
-) {
-    Column {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, top = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(48.dp)
-                    .clip(RoundedCornerShape(Theme.radius.full))
-                    .background(Theme.colors.additional.primary.blue.copy(alpha = 0.08f))
-                    .padding(8.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_waterdrops_bold),
-                    contentDescription = "",
-                    tint = Theme.colors.additional.primary.blue,
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .size(24.dp),
-                )
-            }
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 12.dp, vertical = 4.dp)
-            ) {
-                Text(
-                    text = request.serviceType,
-                    style = Theme.textStyle.body.medium.semibold,
-                    color = Theme.colors.shade.primary
-                )
-                Text(
-                    modifier = Modifier
-                        .padding(top = 8.dp),
-                    text = request.title,
-                    style = Theme.textStyle.body.small.medium,
-                    color = Theme.colors.shade.secondary
-                )
-            }
-        }
-        Text(
-            modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 12.dp),
-            text = request.description,
-            style = Theme.textStyle.body.medium.regular,
-            color = Theme.colors.shade.secondary
-        )
-        RequestInfoIconTextRow(
-            painter = painterResource(id = R.drawable.ic_clock_circle_outline),
-            text = request.time,
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
-        )
-        RequestInfoIconTextRow(
-            painter = painterResource(id = R.drawable.ic_location_outline),
-            text = "${request.location}, ${request.locationDetails}",
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 12.dp),
-            color = Theme.colors.brand.primary
-        )
-
-        if (request.images.isNotEmpty()) {
-            LazyRow(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                contentPadding = PaddingValues(horizontal = 16.dp)
-            ) {
-                items(request.images.size) { index ->
-                    val image = request.images[index]
-                    AsyncImage(
-                        model = image,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(120.dp)
-                            .clip(RoundedCornerShape(Theme.radius.extraLarge))
-                            .background(Theme.colors.background.card),
-                        contentScale = ContentScale.Crop,
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun RequestInfoIconTextRow(
-    painter: Painter,
-    text: String,
-    modifier: Modifier = Modifier,
-    color: Color = Theme.colors.shade.secondary
-) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            painter = painter,
-            contentDescription = null,
-            tint = color,
-            modifier = Modifier.size(16.dp)
-        )
-        Text(
-            text = text,
-            style = Theme.textStyle.body.small.regular,
-            color = color
-        )
-    }
-}
