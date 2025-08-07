72b81775e04ba56edc64670a2f3376e1e8f0315e
a7med naser <ad0112727@gmail.com>
2025-08-07 17:45:43 +0300

    fix the loading of profile step's button

--- app/src/main/java/com/paris_2/san3a/presentation/screen/account/AccountViewModel.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/account/AccountViewModel.kt
@@ -764,16 +764,6 @@ class AccountViewModel(
     override fun onProfileButtonClicked() {
         tryToExecute(
             execute = {
-                updateState(
-                    screenState.value.copy(
-                        accountUiState = screenState.value.accountUiState.copy(
-                            accountButtonState = screenState.value.accountUiState.accountButtonState.copy(
-                                profileButtonState = AppButtonState.Loading
-                            )
-                        )
-                    )
-                )
-
                 val fullName = screenState.value.accountUiState.customerName
                 val profilePhotoUri =
                     screenState.value.accountUiState.customerProfilePhotoUri
@@ -788,11 +778,21 @@ class AccountViewModel(
                         )
                     )
                 }
+                updateState(
+                    screenState.value.copy(
+                        accountUiState = screenState.value.accountUiState.copy(
+                            accountButtonState = screenState.value.accountUiState.accountButtonState.copy(
+                                profileButtonState = AppButtonState.Loading
+                            )
+                        )
+                    )
+                )
                 setUpAccountUseCase.savePersonalInfo(
                     phone = screenState.value.accountUiState.phoneNumber,
                     fullName,
                     profilePhotoUri
                 )
+
             },
             onSuccess = {
                 if (screenState.value.accountUiState.userType == UserType.CRAFTSMAN) {
