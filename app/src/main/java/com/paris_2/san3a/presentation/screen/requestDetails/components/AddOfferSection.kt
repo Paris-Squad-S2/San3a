9a08095c9b3393c9cbea1dd068174acbd440bb16
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-07 21:45:36 +0300

    Refactor AddOfferSection to use AlertDialog for time picker

--- app/src/main/java/com/paris_2/san3a/presentation/screen/requestDetails/components/AddOfferSection.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/requestDetails/components/AddOfferSection.kt
@@ -1,11 +1,11 @@
 package com.paris_2.san3a.presentation.screen.requestDetails.components
 
-import android.app.TimePickerDialog
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DatePicker
@@ -198,7 +198,7 @@ fun AddOfferForm(
     }
 
     if (showTimePicker) {
-        androidx.compose.material3.AlertDialog(
+        AlertDialog(
             onDismissRequest = { onShowTimePickerChange(false) },
             confirmButton = {
                 TextButton(
