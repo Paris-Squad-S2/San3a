4079e740e07e2589b01b711e17fc29505f1eb6bc
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-10 22:47:02 +0300

    Refactor: Display offers count in craftsman home screen
    - Update `CraftsmanHomeScreen` to display `offersCount` instead of `offers.size`.
    - Modify `CraftsmanHomeViewModel` to fetch and update `offersCount` for recent and available jobs.
    - Change `recentRelatedJobs` and `availableJobs` in `CraftsmanHomeUiState` from `List<RequestService>` to `Map<String, RequestServiceUiState>`.
    - Introduce `RequestServiceUiState` to include `offersCount`.
    - Add extension functions `toRequestServiceUiState` and `toRequestServiceUiStateMap` for conversion.

--- app/src/main/java/com/paris_2/san3a/presentation/screen/home/craftsman/CraftsmanHomeUiState.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/home/craftsman/CraftsmanHomeUiState.kt
@@ -1,8 +1,10 @@
 package com.paris_2.san3a.presentation.screen.home.craftsman
 
 import com.paris_2.san3a.domain.entity.RequestService
+import com.paris_2.san3a.domain.entity.RequestStatus
 import com.paris_2.san3a.domain.entity.Stats
 import com.paris_2.san3a.presentation.screen.account.UserType
+import kotlinx.datetime.LocalDateTime
 
 data class CraftsmanHomeState(
     val isLoading: Boolean = false,
@@ -23,6 +25,39 @@ data class CraftsmanHomeUiState(
         earnings = 0.0,
         rating = 0.0
     ),
-    val recentRelatedJobs: List<RequestService> = emptyList(),
-    val availableJobs: List<RequestService> = emptyList(),
+    val recentRelatedJobs: Map<String, RequestServiceUiState> = emptyMap(),
+    val availableJobs: Map<String, RequestServiceUiState> = emptyMap(),
+)
+
+fun RequestService.toRequestServiceUiState(): RequestServiceUiState {
+    return RequestServiceUiState(
+        id = id,
+        userId = userId,
+        requestStatus = requestStatus,
+        title = title,
+        serviceType = serviceType,
+        description = description,
+        location = location,
+        locationDetails = locationDetails,
+        time = time
+    )
+}
+
+fun List<RequestService>.toRequestServiceUiStateMap(): Map<String, RequestServiceUiState> {
+    return this.associate { requestService ->
+        requestService.id to requestService.toRequestServiceUiState()
+    }
+}
+
+data class RequestServiceUiState(
+    val id: String,
+    val userId: String,
+    val requestStatus: RequestStatus,
+    val title: String,
+    val serviceType: String,
+    val description: String,
+    val location: String,
+    val locationDetails: String,
+    val time: LocalDateTime,
+    val offersCount: Int = 0,
 )
