d0f2f6bcc6fce069b1fd520e11335bc58c48c303
a7med naser <ad0112727@gmail.com>
2025-08-15 15:08:55 +0300

    resolve conflicts

--- app/src/main/java/com/paris_2/san3a/presentation/screen/requestDetails/craftsman/CraftsManRequestDetailsScreen.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/requestDetails/craftsman/CraftsManRequestDetailsScreen.kt
@@ -2,6 +2,7 @@ package com.paris_2.san3a.presentation.screen.requestDetails.craftsman
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -30,6 +31,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
