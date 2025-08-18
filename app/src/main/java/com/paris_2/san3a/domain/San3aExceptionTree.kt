4d194d84ef97277977ea0cde6c77ff4a0707f8f5
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-18 23:32:42 +0300

    Refactor: Make FailException final
    The `FailException` class is now final and cannot be subclassed.

--- app/src/main/java/com/paris_2/san3a/domain/San3aExceptionTree.kt
+++ app/src/main/java/com/paris_2/san3a/domain/San3aExceptionTree.kt
@@ -2,4 +2,4 @@ package com.paris_2.san3a.domain
 
 abstract class San3aException(message: String) : Exception(message)
 class NoInternetConnectionException() : San3aException("No internet connection")
-open class FailException(message: String) : San3aException(message)
+class FailException(message: String) : San3aException(message)
