783706fe16b960457b2d24f88538cd155c513140
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-19 16:14:17 +0300

    Refactor: Improve image handling in RequestsRepositoryImpl
    - Use a when statement for clarity when checking if `imageUris` is empty.
    - Remove an unused import `kotlin.collections.map`.
    - Remove an unnecessary blank line at the end of the file.

--- app/src/main/java/com/paris_2/san3a/data/repository/RequestsRepositoryImpl.kt
+++ app/src/main/java/com/paris_2/san3a/data/repository/RequestsRepositoryImpl.kt
@@ -7,16 +7,15 @@ import com.paris_2.san3a.data.repository.shared.BaseRepository
 import com.paris_2.san3a.data.source.remote.requests.RequestRemoteDataSource
 import com.paris_2.san3a.data.source.remote.storage.StorageRemoteDataSource
 import com.paris_2.san3a.data.source.remote.storage.dto.ImageDto
-import com.paris_2.san3a.domain.exceptions.FailException
 import com.paris_2.san3a.domain.entity.Offer
 import com.paris_2.san3a.domain.entity.RequestService
+import com.paris_2.san3a.domain.exceptions.FailException
 import com.paris_2.san3a.domain.repository.RequestsRepository
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
-import kotlin.collections.map
 
 class RequestsRepositoryImpl(
     private val requestRemoteDataSource: RequestRemoteDataSource,
@@ -129,12 +128,13 @@ class RequestsRepositoryImpl(
         validateNetworkConnection()
         safeCall(FailException("requestService")) {
             val imageUris = requestedService.image
-            val imageUrls = if (imageUris.isNotEmpty()) {
-                val images = imageUris.toImageDto(requestedService.title)
-                firebaseStorageRemoteDataSource.saveImages(images)
-                firebaseStorageRemoteDataSource.getImagesByPaths(images)
-            } else {
-                emptyList()
+            val imageUrls = when (imageUris.isNotEmpty()) {
+                true -> {
+                    val images = imageUris.toImageDto(requestedService.title)
+                    firebaseStorageRemoteDataSource.saveImages(images)
+                    firebaseStorageRemoteDataSource.getImagesByPaths(images)
+                }
+                false -> emptyList()
             }
 
             requestRemoteDataSource.requestService(requestedService.toDto(imageUrls))
@@ -157,5 +157,4 @@ class RequestsRepositoryImpl(
             .map { dto -> dto.map { it.toEntity() } }
             .catch { throw FailException("getAvailableJobs failed: ${it.message ?: "Unknown error"}") }
     }
-
 }
