941601032b377ef68b450a25be54dbbeef09f792
mohammed-akkad <momoo362514@gmail.com>
2025-08-19 11:47:52 +0300

    add account app button

---
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/account/components/AccountAppButton.kt
@@ -0,0 +1,37 @@
+package com.paris_2.san3a.presentation.screen.account.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.paris_2.san3a.presentation.shared.components.AppButton
+import com.paris_2.san3a.presentation.shared.components.AppButtonSize
+import com.paris_2.san3a.presentation.shared.components.AppButtonState
+import com.paris_2.san3a.presentation.shared.components.AppButtonType
+import com.paris_2.san3a.presentation.shared.components.LoadingScreen
+import com.paris_2.san3a.presentation.shared.designSystem.theme.Theme
+
+@Composable
+fun AccountAppButton(
+    onClickButton: () -> Unit,
+    textButton: String,
+    state: AppButtonState,
+    modifier: Modifier = Modifier
+) {
+    AppButton(
+        onClick = onClickButton,
+        type = AppButtonType.Primary,
+        text = textButton,
+        state = state,
+        modifier = modifier.fillMaxWidth(),
+        size = AppButtonSize.Large,
+        loadingIcon = {
+            LoadingScreen(
+                modifier = modifier.size(25.dp),
+                background = Theme.colors.button.primary
+            )
+        }
+
+    )
+}
