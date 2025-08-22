2b96cf72350735584aa990988996574c27ca9bf8
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-22 12:03:26 +0300

    Update default profile photo in User entity
    The `profilePhoto` field in the `User` data class now defaults to a specific Firebase Storage URL if the `profilePhoto` value from the data map is null or not present.

--- app/src/main/java/com/paris_2/san3a/domain/entity/User.kt
+++ app/src/main/java/com/paris_2/san3a/domain/entity/User.kt
@@ -18,7 +18,7 @@ data class User(
                 id = id,
                 phone = id,
                 fullName = data["fullName"]?.toString() ?: "",
-                profilePhoto = data["profilePhoto"]?.toString() ?: "",
+                profilePhoto = data["profilePhoto"]?.toString() ?: "https://firebasestorage.googleapis.com/v0/b/cell-monitor21.appspot.com/o/fake%20user%20Img%20don't%20delete%20it.png?alt=media&token=d226452f-3e87-44ef-8a7a-e5e7649fee9f",
                 nationalIdFrontImage = data["nationalIdFrontImage"]?.toString() ?: "",
                 nationalIdBackImage = data["nationalIdBackImage"]?.toString() ?: "",
                 workDescription = data["workDescription"]?.toString() ?: "",
