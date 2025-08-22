d512617e6cbd0687a7987e082836e65caa140106
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-22 12:56:38 +0300

    Refactor CategoryItem to adjust width and improve text overflow
    This commit refactors the `CategoryItem` composable:
    - The `width` modifier is changed to `widthIn(max = 250.dp)` to allow flexible width up to a maximum of 250dp.
    - Text overflow handling for the title is improved by setting `maxLines = 1` and `overflow = TextOverflow.Ellipsis`.

--- app/src/main/java/com/paris_2/san3a/presentation/shared/components/CategoryItem.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/shared/components/CategoryItem.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -63,7 +64,7 @@ fun CategoryItemLarge(
 ){
     Box(
         modifier = modifier
-            .width(220.dp)
+            .widthIn(max = 250.dp)
             .clip(RoundedCornerShape(Theme.radius.tripleXLarge))
             .background(Theme.colors.background.card)
             .clickable { onclick() }
@@ -84,6 +85,8 @@ fun CategoryItemLarge(
                 text = title,
                 style = Theme.textStyle.body.medium.medium,
                 color = Theme.colors.shade.primary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
                 modifier = Modifier.padding(bottom = 4.dp)
             )
             Text(
