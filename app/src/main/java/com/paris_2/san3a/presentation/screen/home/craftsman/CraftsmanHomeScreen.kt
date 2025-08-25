aba25f05c6679fc7cba6723cb8f7b55054bfca3f
a7med naser <ad0112727@gmail.com>
2025-08-25 21:36:36 +0300

    Remove unused import from CraftsmanHomeScreen.kt

--- app/src/main/java/com/paris_2/san3a/presentation/screen/home/craftsman/CraftsmanHomeScreen.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/home/craftsman/CraftsmanHomeScreen.kt
@@ -1,6 +1,5 @@
 package com.paris_2.san3a.presentation.screen.home.craftsman
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
