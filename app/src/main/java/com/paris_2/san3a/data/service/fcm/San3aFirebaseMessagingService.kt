d0c04bb2440d1e31d07229d21b31870dbc751738
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-28 02:35:48 +0300

    Refactor San3aFirebaseMessagingService to suppress lint warnings and comment out token handling logic

--- app/src/main/java/com/paris_2/san3a/data/service/fcm/San3aFirebaseMessagingService.kt
+++ app/src/main/java/com/paris_2/san3a/data/service/fcm/San3aFirebaseMessagingService.kt
@@ -1,5 +1,6 @@
 package com.paris_2.san3a.data.service.fcm
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
@@ -8,13 +9,28 @@ import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.paris_2.san3a.R
+import com.paris_2.san3a.data.source.local.userPreferences.UserPreferencesLocalDataStore
+import com.paris_2.san3a.data.source.remote.user.UserRemoteDataSource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class San3aFirebaseMessagingService: FirebaseMessagingService() {
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class San3aFirebaseMessagingService(): FirebaseMessagingService() {
 
-    override fun onNewToken(token: String) {
-        super.onNewToken(token)
-        Log.d("FCM", "Token: $token")
-    }
+//    override fun onNewToken(token: String) {
+//        super.onNewToken(token)
+//        Log.d("FCM", "Token: $token")
+//        CoroutineScope(Dispatchers.IO).launch {
+//            val userId = userPreferencesLocalDataStore.getPhoneNumber()
+//            if (!userId.isNullOrBlank()) {
+//                userRemoteDataSource.saveDeviceToken(userId, token)
+//            } else {
+//                Log.w("FCM", "No userId found, token not saved")
+//            }
+//        }
+//
+//    }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
