61a6432d434dc825453de47c74f8e933ebb9ea77
Zeinab979 <zeinabwhdan3677@gmail.com>
2025-08-27 22:56:47 +0300

    resizes  layout to the available space, to make content accessible when the keyboard appears on the screen

--- app/src/main/java/com/paris_2/san3a/MainActivity.kt
+++ app/src/main/java/com/paris_2/san3a/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.paris_2.san3a
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         enableEdgeToEdge()
         setContent {
             InstallSavedAppLanguage(this)
--- app/src/main/java/com/paris_2/san3a/presentation/screen/account/AccountScreen.kt
+++ app/src/main/java/com/paris_2/san3a/presentation/screen/account/AccountScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -98,7 +99,9 @@ fun AccountScreenContent(
             .statusBarsPadding()
             .navigationBarsPadding()
             .padding(horizontal = 16.dp, vertical = 16.dp)
-    ) {
+            .imePadding()
+    )
+    {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(16.dp)
