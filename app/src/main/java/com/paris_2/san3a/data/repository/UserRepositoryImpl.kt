fdb8e9f31262a6a9e1f1aa73a2ba29323246f886
a7med naser <ad0112727@gmail.com>
2025-08-28 16:41:13 +0300

    feat: conditional national ID image upload
    The `uploadNationalIdImages` function in `UserRepositoryImpl` now checks if `frontUri` or `backUri` are not null before attempting to upload images. This prevents unnecessary network calls if no images are provided.

--- app/src/main/java/com/paris_2/san3a/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/paris_2/san3a/data/repository/UserRepositoryImpl.kt
@@ -196,21 +196,25 @@ class UserRepositoryImpl(
     override suspend fun uploadNationalIdImages(phone: String, frontUri: Uri?, backUri: Uri?) =
         coroutineScope {
             safeNetworkCall(FailException("Failed to upload national ID images")) {
-                val (frontUrl, backUrl) = listOfNotNull(
-                    frontUri?.let { uri ->
-                        async {
-                            val path = "$NATIONAL_ID_PATH/$phone/$FRONT_IMAGE_NAME"
-                            storageRemoteDataSource.saveImages(listOf(ImageDto(path, uri))).firstOrNull()
+                if (frontUri!=null || backUri!=null) {
+                    val (frontUrl, backUrl) = listOfNotNull(
+                        frontUri?.let { uri ->
+                            async {
+                                val path = "$NATIONAL_ID_PATH/$phone/$FRONT_IMAGE_NAME"
+                                storageRemoteDataSource.saveImages(listOf(ImageDto(path, uri)))
+                                    .firstOrNull()
+                            }
+                        },
+                        backUri?.let { uri ->
+                            async {
+                                val path = "$NATIONAL_ID_PATH/$phone/$BACK_IMAGE_NAME"
+                                storageRemoteDataSource.saveImages(listOf(ImageDto(path, uri)))
+                                    .firstOrNull()
+                            }
                         }
-                    },
-                    backUri?.let { uri ->
-                        async {
-                            val path = "$NATIONAL_ID_PATH/$phone/$BACK_IMAGE_NAME"
-                            storageRemoteDataSource.saveImages(listOf(ImageDto(path, uri))).firstOrNull()
-                        }
-                    }
-                ).awaitAll()
-                userRemoteDataSource.updateNationalIdImages(phone, frontUrl, backUrl)
+                    ).awaitAll()
+                    userRemoteDataSource.updateNationalIdImages(phone, frontUrl, backUrl)
+                }
             }
         }
 
